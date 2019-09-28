deb2eca91870f306ac5a67e6e69e504f144a5805
vannha <phongnhatk21@gmail.com>
2019-09-28 09:02:50 +0700

    commit -

--- assets/fonts/overcome/overcome.php
+++ assets/fonts/overcome/overcome.php
@@ -1,13 +1,13 @@
 <?php
-function overcome_iconpicker_font_name_icons(){
+function overcome_iconpicker_overcome_icons(){
 	// add your icon here
 	// struct 'icon-class-name' => 'icon name'
 	$default_icons = [];
-	return array_merge($default_icons, apply_filters('overcome_iconpicker_font_name_icons', []));
+	return array_merge($default_icons, apply_filters('overcome_iconpicker_overcome_icons', []));
 }
 add_filter( 'vc_iconpicker-type-overcome', 'overcome_vc_iconpicker_type_simple_line_icons' );
 function overcome_vc_iconpicker_type_simple_line_icons( $icons ) {
-	$simple_line_icons = overcome_iconpicker_font_name_icons();
+	$simple_line_icons = overcome_iconpicker_overcome_icons();
 	return array_merge( $icons, $simple_line_icons );
 }
 
@@ -19,20 +19,20 @@ function overcome_vc_iconpicker_type_simple_line_icons( $icons ) {
 	return [
 		'overcome' => [
 			'title'   => 'Font Title ',
-			'icon'    => overcome_iconpicker_font_name_icons(), // icons list
+			'icon'    => overcome_iconpicker_overcome_icons(), // icons list
 			'css'     => get_template_directory_uri() . '/assets/fonts/overcome/overcome.css',
 			'version' => '1.0'
 		]
 	];
  }
- * NOTE: replace 'overcome', 'font_name' with your font name
+ * NOTE: replace 'overcome', 'overcome' with your font name
 */
 add_filter('ef5systems_extra_icons','overcome_default_extra_icons');
 function overcome_default_extra_icons(){
 	return [
 		'overcome' => [
 			'title'   => 'Over Come',
-			'icon'    => overcome_iconpicker_font_name_icons(), // icons list
+			'icon'    => overcome_iconpicker_overcome_icons(), // icons list
 			'css'     => get_template_directory_uri() . '/assets/fonts/overcome/overcome.css',
 			'version' => '1.0'
 		]
