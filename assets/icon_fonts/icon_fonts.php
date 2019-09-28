bbc6694a270622a182a0450fe0d392f4de747a97
vannha <phongnhatk21@gmail.com>
2019-09-28 10:05:20 +0700

    commit -

--- assets/icon_fonts/icon_fonts.php
+++ assets/icon_fonts/icon_fonts.php
@@ -1,6 +1,17 @@
 <?php
 define('OVERCOME_ICON_FONT_DIR' , get_template_directory_uri() . '/assets/icon_fonts/');
 /**
+ * EF5 Systems supported some icon font
+ * like: elegant, ET Line, Flaticon, Linear, Pe7 Stroke, Simple Line
+ * use filter: ef5systems_default_extra_icons
+ * example: 
+ 	add_filter('ef5systems_default_extra_icons','custom_ef5systems_default_extra_icons');
+	function custom_ef5systems_default_extra_icons(){
+		return ['flaticon','linear'];
+	}
+*/
+
+/**
  * Add filter to support your icon font in Mega Menu and VC 
  * user filter: ef5systems_extra_icons
  * @structure add_filter('ef5systems_extra_icons','overcome_extras_icons');
