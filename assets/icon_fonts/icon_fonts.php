e37b3574a896f01896fbb4410e98d9f7700e6c88
vannha <phongnhatk21@gmail.com>
2019-09-28 10:00:14 +0700

    commit -

--- assets/icon_fonts/icon_fonts.php
+++ assets/icon_fonts/icon_fonts.php
@@ -1,31 +1,24 @@
 <?php
+define('OVERCOME_ICON_FONT_DIR' , get_template_directory_uri() . '/assets/icon_fonts/');
 /**
  * Add filter to support your icon font in Mega Menu and VC 
  * user filter: ef5systems_extra_icons
  * @structure add_filter('ef5systems_extra_icons','overcome_extras_icons');
  * function overcome_extras_icons(){
 	return [
-		'overcome' => [
-			'title'   => 'Over Come ',
-			'icon'    => overcome_iconpicker_overcome_icons(), // icons list
-			'css'     => get_template_directory_uri() . '/assets/fonts/overcome/overcome.css',
+		'fontname' => [
+			'title'   => 'Font Name',
+			'icon'    => overcome_iconpicker_fontname_icons(), // icons list
+			'css'     => OVERCOME_ICON_FONT_DIR . 'fontname/fontname.css',
 			'version' => '1.0'
 		]
 	];
  }
- * NOTE: replace 'overcome', 'overcome' with your font name
+ * NOTE: replace 'overcome', 'Font Name' with your font name
 */
-define('OVERCOME_ICON_FONT_DIR' , get_template_directory_uri() . '/assets/icon_fonts/');
-
-add_filter('ef5systems_extra_icons','overcome_extras_icons');
+//add_filter('ef5systems_extra_icons','overcome_extras_icons');
 function overcome_extras_icons(){
 	return [
-		'fontname' => [
-			'title'   => 'Font Name ',
-			'icon'    => overcome_iconpicker_fontname_icons(), // icons list
-			'css'     => OVERCOME_ICON_FONT_DIR.'fontname/fontname.css',
-			'version' => '1.0'
-		],
 		'overcome' => [
 			'title'   => 'Over Come',
 			'icon'    => overcome_iconpicker_overcome_icons(), // icons list
