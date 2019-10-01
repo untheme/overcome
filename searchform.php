0007e975b1527dcb9738e38c059824481123a5e7
vannha <phongnhatk21@gmail.com>
2019-10-01 11:02:34 +0700

    commit -

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url( '/' )); ?>">
     <div class="searchform-wrap">
-        <input type="text" placeholder="<?php esc_attr_e('Type and hit enter ...', 'overcome'); ?>" name="s" class="search-field" />
-        <button type="submit"></button>
+        <input type="text" placeholder="<?php esc_attr_e('Search here...', 'overcome'); ?>" name="s" class="search-field" />
+        <button type="submit"><?php esc_html_e('Search','overcome'); ?></button>
     </div>
 </form>
