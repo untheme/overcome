40a9c09212bb53df35614754c8d8c83e99ec55a5
vannha <phongnhatk21@gmail.com>
2019-10-01 15:55:12 +0700

    commit -

--- functions.php
+++ functions.php
@@ -353,6 +353,7 @@ function overcome_ef5systems_styles(){
         wp_enqueue_style('hint');
     } else {
         wp_enqueue_style('font-awesome5', get_template_directory_uri() . '/assets/icon_fonts/awesome5/css/all.css', array(), wp_get_theme()->get( 'Version' ));
+        wp_enqueue_style('font-overcome', get_template_directory_uri() . '/assets/icon_fonts/overcome/overcome.css', array(), wp_get_theme()->get( 'Version' ));
     }
 }
 
