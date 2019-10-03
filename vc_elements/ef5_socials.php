a56cf43c702a63d5a7a10643980772727ce10ac9
vannha <phongnhatk21@gmail.com>
2019-10-03 11:14:49 +0700

    commit -

---
+++ vc_elements/ef5_socials.php
@@ -0,0 +1,100 @@
+<?php
+vc_map(array(
+    'name'        => 'OverCome Socials',
+    'base'        => 'ef5_socials',
+    'icon'        => '',
+    'category'    => esc_html__('OverCome', 'overcome'),
+    'description' => esc_html__('Add text with icon', 'overcome'),
+    'params'      => array(
+        array(
+            'type'       => 'dropdown',
+            'param_name' => 'source',
+            'heading'    => esc_html__( 'Source', 'overcome' ),
+            'value'      => array(
+                esc_html__( 'Custom', 'overcome' ) => 'custom',
+            ),
+            'std' => 'custom',
+            'description' => esc_html__( 'Choose what social source display.', 'overcome' ),
+            'admin_label' => true,
+        ),
+        array(
+            'type'       => 'img',
+            'heading'    => esc_html__('Layout Template','unbreak'),
+            'param_name' => 'layout_template',
+            'value'      =>  array(
+                '1' => get_template_directory_uri().'/vc_elements/layouts/qc-1.png',
+            ),
+            'std'        => '1',
+            'admin_label' => true,
+        ),
+        array(
+            'type'       => 'dropdown',
+            'param_name' => 'el_icon_hint_pos',
+            'heading'    => esc_html__( 'Hint Label Position', 'overcome' ),
+            'value'      => array(
+                esc_html__( 'Top', 'overcome' )    => 'top',
+                esc_html__( 'Bottom', 'overcome' ) => 'bottom',
+            ),
+            'std' => 'top',
+        ),
+        array(
+            'type'       => 'dropdown',
+            'param_name' => 'el_content_align',
+            'heading'    => esc_html__( 'Content Align', 'overcome' ),
+            'value'      => ef5systems_alignment_option_for_vc(),
+            'std' => '',
+        ),
+        array(
+            'type'       => 'param_group',
+            'heading'    => esc_html__( 'Add your icons', 'overcome' ),
+            'param_name' => 'values',
+            'group'      => esc_html__('Items','overcome'),
+            'dependency' => array(
+                'element' => 'source',
+                'value'   => 'custom',
+            ),
+            'value' => urlencode( json_encode( array(
+                array(
+                    'i_type'             => 'fontawesome',
+                    'i_icon_fontawesome' => 'fa fa-facebook',
+                    'icon_link'          => 'title:Facebook||url:facebook.com'
+                ),
+                array(
+                    'i_type'             => 'fontawesome',
+                    'i_icon_fontawesome' => 'fa fa-twitter',
+                    'icon_link'          => 'title:Twitter||url:twitter.com'
+                ),
+                array(
+                    'i_type'             => 'fontawesome',
+                    'i_icon_fontawesome' => 'fa fa-pinterest',
+                    'icon_link'          => 'title:Pinterest||url:pinterest.com'
+                ),
+                array(
+                    'i_type'             => 'fontawesome',
+                    'i_icon_fontawesome' => 'fa fa-linkedin',
+                    'icon_link'          => 'title:LinkedIn||url:linkedin.com'
+                ),
+            ) ) ),
+            'params'     => array_merge(
+                ef5systems_icon_libs(),
+                ef5systems_icon_libs_icon(),
+                array(
+                    array(
+                        'type'       => 'vc_link',
+                        'heading'    => esc_html__( 'Icon Link', 'overcome' ),
+                        'param_name' => 'icon_link',
+                        'admin_label'=> true,
+                    ),
+                )
+            ),
+        ),
+    )
+));
+
+class WPBakeryShortCode_ef5_socials extends WPBakeryShortCode
+{
+    protected function content($atts, $content = null)
+    {
+        return parent::content($atts, $content);
+    }
+}
---
+++ vc_templates/ef5_socials.php
@@ -0,0 +1,62 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    die( '-1' );
+}
+
+/**
+ * Shortcode attributes
+ * @var $atts
+ * @var $values
+ * Shortcode class
+ * @var $this WPBakeryShortCode_ef5_socials
+ */
+/* get Shortcode custom value */
+    $styles = '';
+    $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+    extract( $atts );
+    $values = (array) vc_param_group_parse_atts( $values );
+    $wrap_css_classes = array(
+        'ef5-social',
+        'ef5-social-'.$layout_template,
+        $el_content_align
+    );
+    $hint_pos = isset($el_icon_hint_pos) ? $el_icon_hint_pos : 'top';
+?>
+<div class="ef5-social-wraps">
+    <div class="<?php echo trim(implode(' ', $wrap_css_classes)); ?>">
+    <?php
+        switch ($source) {
+            case 'custom':
+                foreach($values as $value){
+                    vc_icon_element_fonts_enqueue( $value['i_type'] );  /* Call icon font libs */
+                    $iconClass = isset($value['i_icon_'. $value['i_type']]) ? $value['i_icon_'. $value['i_type']] : ''; /* get icon class */
+                    $link_open = '<a href="javascript:void(0)" data-hint="'.esc_html__('Follow Us','overcome').'">';
+                    $link_close = '</a>';           
+                    if (isset($value['icon_link'])){  
+                        $link = vc_build_link($value['icon_link']);
+                        $link = ( $link == '||' ) ? '' : $link;
+                        if ( strlen( $link['url'] ) > 0 ) {
+                            $a_href    = $link['url'];
+                            $a_title   = isset($link['title']) && !empty($link['title']) ? $link['title'] : esc_html__('Follow Us','overcome');
+                            $a_target  = strlen( $link['target'] ) > 0 ? str_replace(' ', '', $link['target']) : '_blank';
+                            $link_open = '<a class="hint--'.$hint_pos.'" data-hint="'.esc_attr($a_title).'" href="'.esc_url($a_href).'" target="'.esc_attr($a_target).'" '.$styles.'>';
+                            $link_close = '</a>';
+                        }
+                    }
+                    
+                    if($iconClass) {
+                        echo overcome_html($link_open); 
+                        echo '<span class="'.esc_attr($iconClass).'"></span>';
+                        echo overcome_html($link_close); 
+                    }
+                }
+                break;
+             
+            default:
+                break;
+         } 
+    ?>
+    </div>
+</div>
+
+
