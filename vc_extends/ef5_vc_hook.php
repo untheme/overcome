3137caf121c0be8e7bb4c058a40b3a06c2a89ca4
vannha <phongnhatk21@gmail.com>
2019-10-03 16:35:27 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -5,7 +5,7 @@
 add_filter('ef5systems_spacings','overcome_spacings');
 function overcome_spacings(){
 	return [
-		' pt-8 pb-8' => ['OverCome Space 01', 'Top 8px - Bottom 8px', ''],
+		'custom1 pt-8 pb-8' => ['OverCome Space 01', 'Top 8px - Bottom 8px', ''],
 	];
 }
 
