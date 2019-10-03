42324bb88003543672a503211b5cd589d2ea5ebc
vannha <phongnhatk21@gmail.com>
2019-10-03 16:23:21 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -5,8 +5,7 @@
 add_filter('ef5systems_spacings','overcome_spacings');
 function overcome_spacings(){
 	return [
-		'1' => ['OverCome Space 01', '100px 0'],
-		'2' => ['OverCome Space 02', '200px 0']
+		'1' => ['OverCome Space 01', '8px null 8px null'],
 	];
 }
 
