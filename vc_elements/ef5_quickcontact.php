6370dddf43284797807f3c5d4e574eb4256d3010
vannha <phongnhatk21@gmail.com>
2019-10-03 09:31:27 +0700

    commit -

---
+++ vc_elements/ef5_quickcontact.php
@@ -0,0 +1,165 @@
+<?php
+vc_map(array(
+    'name'        => 'OverCome Quick Contact',
+    'base'        => 'ef5_quickcontact',
+    'category'    => esc_html__('OverCome', 'unbreak'),
+    'description' => esc_html__('Add your quick contact info', 'unbreak'),
+    'icon'        => 'icon-wpb-wp',
+    'params'      => array_merge(
+        array(
+            array(
+                'type'       => 'img',
+                'heading'    => esc_html__('Layout Template','unbreak'),
+                'param_name' => 'layout_template',
+                'value'      =>  array(
+                    '1' => get_template_directory_uri().'/vc_elements/layouts/qc-1.png',
+                ),
+                'std'        => '1',
+                'admin_label' => true,
+            ),
+            array(
+                'type'          => 'dropdown',
+                'heading'       => esc_html__('Text Color','unbreak'),
+                'param_name'    => 'content_color',
+                'value'         => ef5systems_colors_option_for_vc(),
+                'std'           => '',
+            ),
+            array(
+                'type'          => 'dropdown',
+                'heading'       => esc_html__('Icon Color','unbreak'),
+                'param_name'    => 'icon_color',
+                'value'         => ef5systems_colors_option_for_vc(),
+                'std'           => '',
+            ),
+            array(
+                'type'          => 'dropdown',
+                'heading'       => esc_html__('Horizontal alignment','unbreak'),
+                'param_name'    => 'content_align',
+                'value'         => ef5systems_alignment_option_for_vc(),
+                'std'           => '',
+                'dependency'    => array(
+                    'element' => 'layout_template',
+                    'value'   => array('1')
+                )
+            ),
+            array(
+                'type'       => 'textfield',
+                'heading'    => esc_html__('Extra Class','unbreak'),
+                'param_name' => 'el_class',
+                'value'      => '',
+                'description' => esc_html__('Style particular content element differently - add a class name and refer to it in custom CSS.', 'unbreak'),
+            ),
+            array(
+                'type'        => 'el_id',
+                'settings' => array(
+                    'auto_generate' => true,
+                ),
+                'heading'     => esc_html__( 'Element ID', 'unbreak' ),
+                'param_name'  => 'el_id',
+                'description' => sprintf( __( 'Enter element ID (Note: make sure it is unique and valid according to <a href="%s" target="_blank">w3c specification</a>).', 'unbreak' ), '//w3schools.com/tags/att_global_id.asp' ),
+            ),
+            array(
+                'type'       => 'textfield',
+                'heading'    => esc_html__('Title','unbreak'),
+                'param_name' => 'el_title',
+                'value'      => '',
+                'dependency' => array(
+                    'element' => 'layout_template',
+                    'value'   => array('2')
+                ),
+                'group'     => 'Contact Info',
+                'holder'    => 'h3'    
+            ),
+            array(
+                'type'       => 'attach_image',
+                'heading'    => esc_html__('Add your Image','unbreak'),
+                'param_name' => 'img_id',
+                'value'      => '',
+                'dependency' => array(
+                    'element' => 'layout_template',
+                    'value'   => array('2')
+                ),
+                'group'     => 'Contact Info'
+            ),
+            array(
+                'type'          => 'textfield',
+                'heading'       => esc_html__('Images size','unbreak'),
+                'description'   => esc_html__('Enter image size (Example: "thumbnail", "medium", "large","post-thumbnail", "full". Alternatively enter size in pixels (Example: 200x100 (Width x Height)). Leave blank to use default','unbreak'),
+                'param_name'    => 'img_size',
+                'value'         => '',
+                'std'           => '',
+                'dependency' => array(
+                    'element'   => 'img_id',
+                    'not_empty' => true
+                ),
+                'group'         => esc_html__('Contact Info','unbreak'),
+            ),
+            array(
+                'type'       => 'param_group',
+                'heading'    => esc_html__( 'Add your info', 'unbreak' ),
+                'param_name' => 'values',
+                'value' => urlencode( json_encode( array(
+                    array(
+                        'i_type'             => 'fontawesome',
+                        'i_icon_fontawesome' => 'fa fa-phone',
+                        'qc_heading'         => '(+88)222.888.66',
+                        'qc_text'            => 'Free call',
+                    ),
+                    array(
+                        'i_type'             => 'fontawesome',
+                        'i_icon_fontawesome' => 'fa fa-clock-o',
+                        'qc_heading'         => '8:00 AM - 6:00 PM',
+                        'qc_text'            => 'Monday - Friday',
+                    ),
+                    array(
+                        'i_type'             => 'fontawesome',
+                        'i_icon_fontawesome' => 'fa fa-map-marker',
+                        'qc_heading'         => '99 Kellen Motorway',
+                        'qc_text'            => 'Wallis and Futuna',
+                    )
+                ) ) ),
+                'params'     => array_merge(
+                    ef5systems_icon_libs(),
+                    ef5systems_icon_libs_icon(),
+                    array(
+                        array(
+                            'type'        => 'textarea',
+                            'heading'     => esc_html__( 'Heading', 'unbreak' ),
+                            'param_name'  => 'qc_heading',
+                            'admin_label' => true,
+                        ),
+                        array(
+                            'type'        => 'textarea',
+                            'heading'     => esc_html__( 'Text', 'unbreak' ),
+                            'param_name'  => 'qc_text',
+                            'admin_label' => true,
+                        ),
+                    )
+                ),
+                'group'     => 'Contact Info'
+            ),
+            array(
+                'type'        => 'vc_link',
+                'param_name'  => 'get_direction',
+                'heading'     => esc_html__('Direction with Map','unbreak'),
+                'description' => esc_html__('Add your map','unbreak'),
+                'value'       => 'title:Get Direction||url:google.com/maps/dir//Bobst+Library,+70+Washington+Square+S,+New+York,+NY+10012,+USA/@40.7291313,-73.9972548,18.22z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c2599051e43f09:0x73708ab233ccf977!2m2!1d-73.9971753!2d40.7294556||target:_blank',
+                'dependency' => array(
+                    'element' => 'layout_template',
+                    'value'   => array('2')
+                ),
+                'group'       => esc_html__('Maps','unbreak')
+            )
+        )
+    )
+));
+
+class WPBakeryShortCode_ef5_quickcontact extends WPBakeryShortCode
+{
+    protected function content($atts, $content = null)
+    {
+        $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+        extract( $atts );
+        return parent::content($atts, $content);
+    }
+}
---
+++ vc_templates/ef5_quickcontact.php
@@ -0,0 +1,103 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    die( '-1' );
+}
+/**
+ * Shortcode attributes
+ * @var $atts
+ * @var $values
+ * Shortcode class
+ * @var $this WPBakeryShortCode_ef5_quickcontact
+ */
+/* get Shortcode custom value */
+$atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+extract( $atts );
+$wrap_css_class = [
+    'ef5-qc', 
+    'ef5-qc-'.$layout_template, 
+    $content_align, 
+    $content_color,
+    $el_class 
+];
+$wrap_inner_css_class = ['row'];
+$item_class = ['qc-item'];
+$item_inner_class = ['row'];
+$qc_infos = vc_map_get_attributes( $this->getShortcode(), $atts );
+$values = (array) vc_param_group_parse_atts( $qc_infos['values'] );
+if(empty($values[0])) {
+    echo '<p class="require required">'.esc_html__('Please add your contact info','unbreak').'</p>';
+    return;
+}
+$qc_icon = $qc_text = '';
+$icon_color = !empty($icon_color) ? $icon_color : 'accent-color';
+$icon_classes = ['qc-icon', $icon_color];
+switch ($layout_template) {
+    case '4':
+        $wrap_inner_css_class[] = 'gutters-60';
+        $item_class[] = 'col-auto';
+        $item_inner_class[] = 'gutters-10';
+        break;
+    case '2':
+        $item_class[] = 'col-12';
+        $item_inner_class[] = 'gutters-20';
+        break;
+    default:
+        $wrap_inner_css_class[] = 'gutters-80';
+        $item_class[] = 'col-auto';
+        $item_inner_class[] = 'gutters-20 align-items-center';
+        break;
+}
+// Get Direction
+$use_link = false;
+$get_direction = '';
+if(!empty($atts['get_direction'])){
+    $button_link = vc_build_link( $atts['get_direction'] );
+    $button_link = ( $button_link == '||' ) ? '' : $button_link;
+    if ( strlen( $button_link['url'] ) > 0 ) {
+        $use_link = true; 
+        $a_href = $button_link['url'];
+        $a_title = strlen($button_link['title']) > 0 ? $button_link['title'] : esc_html__('Read More','unbreak') ;
+        $a_target = strlen( $button_link['target'] ) > 0 ? $button_link['target'] : '_self';
+        // Button for layout 4
+        $get_direction = '<div class="qc-direction ef5-btn-wrap col-12"><a class="ef5-btn-link text-accent simple ef5-btn-df icon-right underline-primary transition ef5-scroll" href="'.esc_url($a_href).'" target="'.esc_attr($a_target).'"><span class="btn-title">'.esc_html($a_title).'&nbsp;&nbsp;</span> <span class="btn-icon flaticon-right-arrow"></span></a></div>';
+    }
+}
+?>
+<div id="<?php echo esc_attr($el_id);?>" class="<?php echo trim(implode(' ', $wrap_css_class));?>">
+    <div class="<?php echo trim(implode(' ', $wrap_inner_css_class));?>">
+        <?php
+            ob_start();
+
+            if(!empty($img_id)) unbreak_image_by_size([
+                'id'     => $img_id, 
+                'size'   => '270x184',
+                'class'  => 'image-fit',
+                'before' =>'<div class="qc-image col-12">',
+                'after'  => '</div>'
+            ]);
+        	if(!empty($el_title)) echo '<div class="ef5-heading qc-heading col-12 text-20">'.esc_html($el_title).'</div>';
+            foreach($values as $value){
+                vc_icon_element_fonts_enqueue( $value['i_type'] );
+                $iconClass  = isset($value['i_icon_'. $value['i_type']]) ? $value['i_icon_'. $value['i_type']] : '';
+                $qc_icon    = !empty($iconClass) ? '<span class="'.implode(' ', $icon_classes).' '.$iconClass.'"></span>' : '';
+                $qc_heading = isset($value['qc_heading']) && !empty($value['qc_heading']) ? $value['qc_heading'] : '';
+                $qc_text    = isset($value['qc_text']) ? $value['qc_text'] : '';
+                switch ($layout_template) {
+                    default:
+                        echo '<div class="'.trim(implode(' ', $item_class)).'">';
+                            echo '<div class="'.trim(implode(' ', $item_inner_class)).'">';
+                                if(!empty($iconClass)) echo '<div class="col-auto">'.unbreak_html($qc_icon).'</div>';
+                                echo '<div class="col">';
+                                    echo '<div class="ef5-heading qc-item-heading font-style-600">'.unbreak_html($qc_heading).'</div>';
+                                    if(!empty($qc_text)) echo '<div class="qc-text">'.unbreak_html($qc_text).'</div>';
+                                echo '</div>';
+                            echo '</div>';
+                        echo '</div>';
+                    break;
+                }
+            }
+            if($layout_template === '2') echo unbreak_html($get_direction);
+            echo ob_get_clean();
+        ?>
+    </div>
+</div>
