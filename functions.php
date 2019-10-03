0ecf7aec89392a132b9b4ef9f253556139f5c51b
vannha <phongnhatk21@gmail.com>
2019-10-03 11:22:17 +0700

    commit -

--- functions.php
+++ functions.php
@@ -350,6 +350,7 @@ function overcome_ef5systems_styles(){
     if(wp_script_is('font-awesome5')){
         // Call libs css
         wp_enqueue_style('font-awesome5');
+        wp_enqueue_style('font-awesome5-shim');
         wp_enqueue_style('hint');
     } else {
         wp_enqueue_style('font-awesome5', get_template_directory_uri() . '/assets/icon_fonts/awesome5/css/all.css', array(), wp_get_theme()->get( 'Version' ));
