90193c5e119022a1a8c945dd121f76458ba2ab02
vannha <phongnhatk21@gmail.com>
2019-10-03 14:49:13 +0700

    commit -

--- vc_elements/ef5_socials.php
+++ vc_elements/ef5_socials.php
@@ -6,104 +6,103 @@ vc_map(array(
     'icon'        => '',
     'category'    => esc_html__('OverCome', 'overcome'),
     'description' => esc_html__('Add text with icon', 'overcome'),
-    'params'      => array(
+    'params'      => array_merge(
         array(
-            'type'       => 'dropdown',
-            'param_name' => 'source',
-            'heading'    => esc_html__( 'Source', 'overcome' ),
-            'value'      => array(
-                esc_html__( 'Custom', 'overcome' ) => 'custom',
+            array(
+                'type'       => 'dropdown',
+                'param_name' => 'source',
+                'heading'    => esc_html__( 'Source', 'overcome' ),
+                'value'      => array(
+                    esc_html__( 'Custom', 'overcome' ) => 'custom',
+                ),
+                'std' => 'custom',
+                'description' => esc_html__( 'Choose what social source display.', 'overcome' ),
+                'admin_label' => true,
             ),
-            'std' => 'custom',
-            'description' => esc_html__( 'Choose what social source display.', 'overcome' ),
-            'admin_label' => true,
-        ),
-        array(
-            'type'       => 'img',
-            'heading'    => esc_html__('Layout Template','unbreak'),
-            'param_name' => 'layout_template',
-            'value'      =>  array(
-                '1' => get_template_directory_uri().'/vc_elements/layouts/qc-1.png',
+            array(
+                'type'       => 'img',
+                'heading'    => esc_html__('Layout Template','unbreak'),
+                'param_name' => 'layout_template',
+                'value'      =>  array(
+                    '1' => get_template_directory_uri().'/vc_elements/layouts/qc-1.png',
+                ),
+                'std'        => '1',
+                'admin_label' => true,
             ),
-            'std'        => '1',
-            'admin_label' => true,
+            array(
+                'type'       => 'textfield',
+                'param_name' => 'el_title',
+                'heading'    => esc_html__( 'Element Title', 'overcome' ),
+                'value'      => '',
+                'description' => esc_html__( 'Enter element title', 'overcome' ),
+                'admin_label' => true,
+            )
         ),
-        array(
-            'type'       => 'textfield',
-            'param_name' => 'el_title',
-            'heading'    => esc_html__( 'Element Title', 'overcome' ),
-            'value'      => '',
-            'description' => esc_html__( 'Enter element title', 'overcome' ),
-            'admin_label' => true,
-        ),
-        ef5systems_icon_libs([
-            'group'            => '',
-            'field_prefix'     => 'i_', 
-            'dependency'       => '',
-            'dependency_value' => ''
-        ]),
+        ef5systems_icon_libs(),
         ef5systems_icon_libs_icon(),
         array(
-            'type'       => 'dropdown',
-            'param_name' => 'el_icon_hint_pos',
-            'heading'    => esc_html__( 'Hint Label Position', 'overcome' ),
-            'value'      => array(
-                esc_html__( 'Top', 'overcome' )    => 'top',
-                esc_html__( 'Bottom', 'overcome' ) => 'bottom',
+            array(
+                'type'       => 'dropdown',
+                'param_name' => 'el_icon_hint_pos',
+                'heading'    => esc_html__( 'Hint Label Position', 'overcome' ),
+                'value'      => array(
+                    esc_html__( 'Top', 'overcome' )    => 'top',
+                    esc_html__( 'Bottom', 'overcome' ) => 'bottom',
+                ),
+                'std' => 'top',
             ),
-            'std' => 'top',
-        ),
-        array(
-            'type'       => 'dropdown',
-            'param_name' => 'el_content_align',
-            'heading'    => esc_html__( 'Content Align', 'overcome' ),
-            'value'      => ef5systems_alignment_option_for_vc(),
-            'std'        => '',
-        ),
-        array(
-            'type'       => 'param_group',
-            'heading'    => esc_html__( 'Add your icons', 'overcome' ),
-            'param_name' => 'values',
-            'group'      => esc_html__('Items','overcome'),
-            'dependency' => array(
-                'element' => 'source',
-                'value'   => 'custom',
+            array(
+                'type'       => 'dropdown',
+                'param_name' => 'el_content_align',
+                'heading'    => esc_html__( 'Content Align', 'overcome' ),
+                'value'      => ef5systems_alignment_option_for_vc(),
+                'std'        => '',
             ),
-            'value' => urlencode( json_encode( array(
-                array(
-                    'i_type'             => 'fontawesome',
-                    'i_icon_fontawesome' => 'fa fa-facebook',
-                    'icon_link'          => 'title:Facebook||url:facebook.com'
+            array(
+                'type'       => 'param_group',
+                'heading'    => esc_html__( 'Add your icons', 'overcome' ),
+                'param_name' => 'values',
+                'group'      => esc_html__('Items','overcome'),
+                'dependency' => array(
+                    'element' => 'source',
+                    'value'   => 'custom',
                 ),
-                array(
-                    'i_type'             => 'fontawesome',
-                    'i_icon_fontawesome' => 'fa fa-twitter',
-                    'icon_link'          => 'title:Twitter||url:twitter.com'
-                ),
-                array(
-                    'i_type'             => 'fontawesome',
-                    'i_icon_fontawesome' => 'fa fa-pinterest',
-                    'icon_link'          => 'title:Pinterest||url:pinterest.com'
-                ),
-                array(
-                    'i_type'             => 'fontawesome',
-                    'i_icon_fontawesome' => 'fa fa-linkedin',
-                    'icon_link'          => 'title:LinkedIn||url:linkedin.com'
-                ),
-            ) ) ),
-            'params'     => array_merge(
-                ef5systems_icon_libs(),
-                ef5systems_icon_libs_icon(),
-                array(
+                'value' => urlencode( json_encode( array(
                     array(
-                        'type'       => 'vc_link',
-                        'heading'    => esc_html__( 'Icon Link', 'overcome' ),
-                        'param_name' => 'icon_link',
-                        'admin_label'=> true,
+                        'i_type'             => 'fontawesome',
+                        'i_icon_fontawesome' => 'fa fa-facebook',
+                        'icon_link'          => 'title:Facebook||url:facebook.com'
                     ),
-                )
-            ),
-        ),
+                    array(
+                        'i_type'             => 'fontawesome',
+                        'i_icon_fontawesome' => 'fa fa-twitter',
+                        'icon_link'          => 'title:Twitter||url:twitter.com'
+                    ),
+                    array(
+                        'i_type'             => 'fontawesome',
+                        'i_icon_fontawesome' => 'fa fa-pinterest',
+                        'icon_link'          => 'title:Pinterest||url:pinterest.com'
+                    ),
+                    array(
+                        'i_type'             => 'fontawesome',
+                        'i_icon_fontawesome' => 'fa fa-linkedin',
+                        'icon_link'          => 'title:LinkedIn||url:linkedin.com'
+                    ),
+                ) ) ),
+                'params' => array_merge(
+                    ef5systems_icon_libs(),
+                    ef5systems_icon_libs_icon(),
+                    array(
+                        array(
+                            'type'       => 'vc_link',
+                            'heading'    => esc_html__( 'Icon Link', 'overcome' ),
+                            'param_name' => 'icon_link',
+                            'admin_label'=> true,
+                        ),
+                    )
+                ),
+            )
+        )
     )
 ));
 
