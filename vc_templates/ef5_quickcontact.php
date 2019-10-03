6d52e841e8bdb6741ee18a5fae412dc73162989e
vannha <phongnhatk21@gmail.com>
2019-10-03 11:39:12 +0700

    commit -

--- vc_templates/ef5_quickcontact.php
+++ vc_templates/ef5_quickcontact.php
@@ -26,6 +26,7 @@ switch ($layout_template) {
     case '1':
         $item_inner_class[] = 'gutter-10 align-items-center';
         $wrap_css_class[] = 'text-12 font-style-500';
+        $item_class[] = 'col-12';
     break;
 }
 $qc_infos = vc_map_get_attributes( $this->getShortcode(), $atts );
