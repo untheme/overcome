e278aca56865060e3d72f3c2b52a8a998bdc9a1f
vannha <phongnhatk21@gmail.com>
2019-10-07 11:37:41 +0700

    commit -

--- inc/functions/header-donate.php
+++ inc/functions/header-donate.php
@@ -4,7 +4,7 @@ function overcome_header_donate_button(){
 	var_dump(ef5payments_default_donation());
 	if(!function_exists('ef5payments_donation_donate_button') || overcome_get_opts('header_donate', '0') === '0') return;
 	ef5payments_donation_donate_button([
-		'id'    => ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation')),
+		'id'    => 98, //ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation')),
 		'title' => overcome_get_opts('header_donate_label', esc_html__('Donate Now','overcome'))
 	]);
 }
