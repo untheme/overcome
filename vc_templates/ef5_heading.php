ef77b266a5216603e076ee68d97aac228fdc32c8
vannha <phongnhatk21@gmail.com>
2019-10-07 16:25:28 +0700

    commit -

--- vc_templates/ef5_heading.php
+++ vc_templates/ef5_heading.php
@@ -17,8 +17,8 @@ extract( $atts );
 			default:
 				$this->ef5_heading_small_heading($atts);
 				$this->ef5_heading_main_heading($atts,['class' => 'text-22 text-uppercase text-accent']);
-				$this->ef5_heading_sub_heading($atts,['class' => 'text-36 font-style-700 ef5-heading']);
-				$this->ef5_heading_desccription($atts);
+				$this->ef5_heading_sub_heading($atts);
+				$this->ef5_heading_desccription($atts,['class' => 'text-36 font-style-700 ef5-heading']);
 				$this->ef5_heading_button($atts);
 			break;
 		}
