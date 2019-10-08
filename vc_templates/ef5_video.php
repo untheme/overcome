73380c3a8a141bd88869e379ff58499f540865fb
vannha <phongnhatk21@gmail.com>
2019-10-08 11:15:13 +0700

    commit -

--- vc_templates/ef5_video.php
+++ vc_templates/ef5_video.php
@@ -1,6 +1,7 @@
 <?php
     $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
     extract( $atts );
+    $video_class = ['ef5-video'];
 ?>
 <div class="ef5-video-wrapper ef5-video-<?php echo esc_attr($layout_template);?>">
 <div id="ef5-video-<?php echo esc_attr($el_id);?>">
@@ -13,7 +14,7 @@
             $this->overcome_ef5_video_desc($atts);
             $this->overcome_ef5_video_link($atts);
         ?></div>
-        <div class="ef5-video <?php echo esc_attr($video_class);?>"><?php 
+        <div class="<?php echo trim(implode(' ', $video_class));?>"><?php 
             $this->overcome_ef5_video_poster($atts);
             //$this->overcome_ef5_video_play_button($atts);
             $this->overcome_ef5_video_plain($atts);
