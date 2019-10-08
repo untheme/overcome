ca070a7c147c61119425180d9358c0a233b4bd67
vannha <phongnhatk21@gmail.com>
2019-10-08 08:55:31 +0700

    commit -

---
+++ vc_elements/ef5_list.php
@@ -0,0 +1,108 @@
+<?php
+vc_map(array(
+    'name'        => 'OverCome List',
+    'base'        => 'ef5_list',
+    'icon'        => '',
+    'category'    => esc_html__('OverCome', 'overcome'),
+    'description' => esc_html__('Add list with icon', 'overcome'),
+    'params'      => array_merge(
+        array(
+            array(
+                'type'       => 'img',
+                'heading'    => esc_html__('Layout Template','unbreak'),
+                'param_name' => 'layout_template',
+                'value'      =>  array(
+                    '1' => get_template_directory_uri().'/vc_elements/layouts/list-1.png',
+                ),
+                'std'         => '1',
+                'admin_label' => true,
+            ),
+            array(
+                'type'        => 'textfield',
+                'param_name'  => 'el_title',
+                'heading'     => esc_html__( 'Element Title', 'overcome' ),
+                'value'       => 'Our strategic priorities up to 2019 are:',
+                'std'         => 'Our strategic priorities up to 2019 are:',
+                'description' => esc_html__( 'Enter element title', 'overcome' ),
+                'admin_label' => true,
+            )
+        ),
+        ef5systems_icon_libs([
+            'group'        => '',
+            'field_prefix' => 'title_icon_',
+            'heading'      => esc_html__('Title Icon','overcome')
+        ]),
+        ef5systems_icon_libs_icon([
+            'group'        => '',
+            'field_prefix' => 'title_icon_',
+        ]),
+        array(
+            array(
+                'type'       => 'param_group',
+                'heading'    => esc_html__( 'Add your lists', 'overcome' ),
+                'param_name' => 'values',
+                'group'      => esc_html__('Lists','overcome'),
+                'value' => urlencode( json_encode( array(
+                    array(
+                        'i_type'             => 'overcome',
+                        'i_icon_fontawesome' => 'flaticon-right-arrow-forward',
+                        'text'               => 'Protecting charities from abuse or mismanagement'
+                    ),
+                    array(
+                        'i_type'             => 'overcome',
+                        'i_icon_fontawesome' => 'flaticon-right-arrow-forward',
+                        'text'               => 'Enabling trustees to run their charities effectively'
+                    ),
+                    array(
+                        'i_type'             => 'overcome',
+                        'i_icon_fontawesome' => 'flaticon-right-arrow-forward',
+                        'text'               => 'Encouraging greater transparency and accountability'
+                    )
+                ) ) ),
+                'params' => array_merge(
+                    ef5systems_icon_libs(),
+                    ef5systems_icon_libs_icon(),
+                    array(
+                        array(
+                            'type'       => 'textfield',
+                            'heading'    => esc_html__( 'Your text', 'overcome' ),
+                            'param_name' => 'text',
+                            'admin_label'=> true,
+                        ),
+                    )
+                ),
+            )
+        ),
+        // Design options
+        array(
+            array(
+                'type'         => 'dropdown',
+                'heading'      => esc_html__( 'Inner Space (Padding)', 'overcome' ),
+                'param_name'   => 'ef5_padding',
+                'value'        => ef5systems_spacing_option_for_vc(),
+                'std'          => 'default',
+                'description'  => esc_html__( 'Choose inner space', 'overcome' ),
+                'group'        => esc_html__('Design Options','overcome'),
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+            array(
+                'type'         => 'dropdown',
+                'heading'      => esc_html__( 'Outer Space (Margin)', 'overcome' ),
+                'param_name'   => 'ef5_margin',
+                'value'        => ef5systems_spacing_option_for_vc(),
+                'std'          => 'default',
+                'description'  => esc_html__( 'Choose outer space', 'overcome' ),
+                'group'        => esc_html__('Design Options','overcome'),
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+        )
+    )
+));
+
+class WPBakeryShortCode_ef5_list extends WPBakeryShortCode
+{
+    protected function content($atts, $content = null)
+    {
+        return parent::content($atts, $content);
+    }
+}
---
+++ vc_templates/ef5_list.php
@@ -0,0 +1,53 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    die( '-1' );
+}
+
+/**
+ * Shortcode attributes
+ * @var $atts
+ * @var $values
+ * Shortcode class
+ * @var $this WPBakeryShortCode_ef5_lists
+ */
+/* get Shortcode custom value */
+    $styles = '';
+    $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+    extract( $atts );
+    $values = (array) vc_param_group_parse_atts( $values );
+    $wrap_css_classes = array(
+        'ef5-list',
+        'ef5-list-'.$layout_template
+    );
+    if($ef5_padding !== 'default') $wrap_css_classes[] = 'ef5-padding-'.$ef5_padding;
+    if($ef5_margin !== 'default') $wrap_css_classes[]  = 'ef5-padding-'.$ef5_margin;
+    $hint_pos = isset($el_icon_hint_pos) ? $el_icon_hint_pos : 'top';
+    // Title
+    if(!empty($el_title)){
+        vc_icon_element_fonts_enqueue($title_icon_type);
+        $title_iconClass = ${'title_icon_icon_'.$title_icon_type};
+        $el_title_icon = !empty($title_iconClass) ? '<span class="title-icon '.$title_iconClass.'"></span>' : '';
+        $title = '<span class="ef5-el-title">'.$el_title_icon.$el_title.'</span>';
+    }
+?>
+<div class="ef5-list-wraps">
+    <div class="<?php echo trim(implode(' ', $wrap_css_classes)); ?>">
+    <?php
+        if(!empty($el_title)) echo overcome_html($title);
+        foreach($values as $value){
+            vc_icon_element_fonts_enqueue( $value['i_type'] );  /* Call icon font libs */
+            $iconClass = isset($value['i_icon_'. $value['i_type']]) ? $value['i_icon_'. $value['i_type']] : ''; /* get icon class */
+            if(!empty($text)){
+                $output = '<div class="ef5-list-item">';
+                if($iconClass) {
+                    $output .= '<span class="'.esc_attr($iconClass).'"></span>';
+                }
+                $output .= overcome_html($text);
+                $output .= '</div>';
+            }
+        }
+    ?>
+    </div>
+</div>
+
+
