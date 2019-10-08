42aa800effbb83f08e21f470ba837058f887abcf
vannha <phongnhatk21@gmail.com>
2019-10-08 15:01:07 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -16,8 +16,8 @@ function overcome_spacings(){
 add_filter('ef5systems_gutters','overcome_gutters');
 function overcome_gutters(){
 	return [
-		'20' => ['OverCome Gutter 20'],
-		'25' => ['OverCome Gutter 25'],
+		'20' => ['OverCome Gutter 20', '20px','200px'],
+		'25' => ['OverCome Gutter 25', '25px','250px'],
 	];
 }
 
