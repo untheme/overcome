2eedcfd7e0d46738836c203fa8452cc66fbd7807
vannha <phongnhatk21@gmail.com>
2019-10-10 15:45:32 +0700

    commit -

--- vc_templates/ef5_posts_carousel.php
+++ vc_templates/ef5_posts_carousel.php
@@ -34,9 +34,6 @@
     $grid_item_css_class = ['ef5-post-item ef5-carousel-item', $this->getCSSAnimation( $css_animation )];
     // Items CSS Classes
     $item_css_class = ['post-item','transition', 'ef5-hover-wrap'];
-    
-    // Title 
-    $title_class = '';
 
     // Thumbnail Size
     $thumbnail_size_index = -1;
@@ -57,26 +54,6 @@
             $post_metas   = [];
             $post_metas[] = overcome_posted_on(['show_date'=>'1','echo' => false]);
             $post_metas[] = overcome_posted_by(['show_author'=>'1','author_avatar' => false, 'echo' => false]);
-            /**
-             * Layout 5 Post Metas 
-            */
-            $post_metas_5   = [];
-            $post_metas_5[] = overcome_posted_by(['show_author'=>'1','author_avatar' => false, 'echo' => false]);
-            $post_metas_5[] = overcome_comments_popup_link(['show_cmt'=>'1','echo' => false]);
-
-            // Readmore button 
-            $overcome_post_media_readmore = overcome_post_read_more(['echo' => false, 'show_readmore' => '1', 'readmore_class' => ' center-align']);
-            $overcome_post_media_readmore2 = overcome_post_read_more_circle(['echo' => false, 'show_readmore' => '1', 'readmore_class' => 'center-align']);
-            $overcome_post_media_readmore3 = overcome_post_read_more_circle([
-                'echo'    => false,
-                'class'   => 'sonarWarning', 
-                'size'    => '50',
-                'bgcolor' => 'bg-accent',
-                'icon'    => 'flaticon-right-arrow-1 text-white',
-            ]);
-            $overcome_post_media_cat   = overcome_posted_in(['echo' => false, 'show_cat' => '1','class' => 'ef5-box-meta ef5-box-meta2', 'sep' => '']);
-            $overcome_post_media_share = overcome_post_share(['echo' => false, 'class' => 'col-auto', 'show_share' => '0', 'show_title' => false, 'social_args' => ['class' => 'shape-circle colored-hover outline justify-content-center', 'size' => '30']]);
-            $overcome_post_media_date  = overcome_posted_on(['echo' => false, 'show_date' => '1','class' => 'ef5-box-meta ef5-box-meta2 text-uppercase', 'sep' => '']);
         ?>
         <div class="<?php echo trim(implode(' ',$grid_item_css_class )); ?>" style="animation-delay: <?php echo esc_html($d*100);?>ms">
         <?php
@@ -119,27 +96,5 @@
         ef5systems_owl_dots_container($atts);
         ef5systems_owl_nav_container($atts);
         ef5systems_owl_dots_in_nav_container($atts);
-        
-        $view_all_class = ['ef5-btn-link simple icon-right transition d-inline-block'];
-        $view_all_icon_class = ['btn-icon flaticon-right-arrow'];
-        $view_all_style = isset($view_all_style) ? $view_all_style : '1';
-        switch ($view_all_style) {
-            case '2':
-                $view_all_class[] = 'ef5-primary-color';
-                $view_all_icon_class[] = 'text-accent';
-                break;
-            
-            default:
-                $view_all_class[] = 'ef5-accent-color';
-                $view_all_icon_class[] = 'text-t999999';
-                break;
-        }
-        if($show_view_all && !empty($show_view_all_page)){
-            echo '<div class="ef5-posts-view-all"><a class="'.implode(' ', $view_all_class).'" href="'.esc_url(get_permalink($show_view_all_page)).'"><span class="btn-title">'
-                .sprintf(
-                    '%s', 
-                    $show_view_all_text
-                ).'</span><span class="'.implode(' ', $view_all_icon_class).'"></span></a></div>';
-        }
     ?></div>
 </div>
