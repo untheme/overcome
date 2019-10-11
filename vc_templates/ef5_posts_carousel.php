5132f49da472e14eec0f7483945da011f90538a9
vannha <phongnhatk21@gmail.com>
2019-10-11 17:23:05 +0700

    commit -

--- vc_templates/ef5_posts_carousel.php
+++ vc_templates/ef5_posts_carousel.php
@@ -74,7 +74,7 @@
                     overcome_post_media([
                         'thumbnail_size' => $thumbnail_size[$thumbnail_size_index], 
                         'default_thumb'  => true,
-                        'after'          => '<div class="overlay"><div class="overlay-inner">'.$overlay.'</div></div>'
+                        'after'          => '<div class="hover-content"><div class="overlay-inner">'.$overlay.'</div></div>'
                     ]);
                 ?>
                 <div class="ef5-post-info">
