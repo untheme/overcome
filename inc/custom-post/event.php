49aaa55dd72343f89e207638cdcb86e2c9c076a0
vannha <phongnhatk21@gmail.com>
2019-10-12 17:44:51 +0700

    commit -

--- inc/custom-post/event.php
+++ inc/custom-post/event.php
@@ -42,3 +42,10 @@ function overcome_cpts_event_tax($taxo) {
 	}
     return $taxo;
 }
+
+// Support Payment 
+add_filter ('ef5payments_post_type_support','ef5payments_post_type_event');
+function ef5payments_post_type_event($post_type){
+	$post_type[] = 'ef5_event';
+	return $post_type;
+}
