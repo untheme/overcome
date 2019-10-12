bd98ffa88d6c047c6e0a3ae4279ccb4ae0116520
chinhexp <chinh@joomexp.com>
2019-10-12 23:12:06 +0700

    commit -

--- inc/custom-post/tribe_events.php
+++ inc/custom-post/tribe_events.php
@@ -10,7 +10,7 @@ add_filter('ef5payments_post_type_support','ef5payments_post_type_tribe_events')
 add_filter('ef5payments_metabox_attach_post_types','ef5payments_post_type_tribe_events');
 add_filter('ef5payments_payment_attach_post_types','ef5payments_post_type_tribe_events');
 
-function ef5payments_post_type_event($post_type){
+function ef5payments_post_type_tribe_events($post_type){
 	$post_type[] = 'tribe_events';
 	return $post_type;
 }
