533e5ca2dcb2b8d1378d23a78601df005e09f425
vannha <phongnhatk21@gmail.com>
2019-10-12 11:20:30 +0700

    commit -

--- vc_templates/ef5_heading.php
+++ vc_templates/ef5_heading.php
@@ -17,10 +17,10 @@ extract( $atts );
 			default:
 				$this->ef5_heading_small_heading($atts, ['class' => 'text-22 text-uppercase ef5-text-accent']);
 				$this->ef5_heading_main_heading_icon($atts);
-				$this->ef5_heading_main_heading($atts,['class' => 'text-36 font-style-700 ef5-heading']);
-				$this->ef5_heading_sub_heading($atts, ['class' => 'text-22']);
+				$this->ef5_heading_main_heading($atts,['class' => 'text-48 font-style-700 ef5-heading ef5-text-white']);
+				$this->ef5_heading_sub_heading($atts, ['class' => 'text-22 ef5-text-white']);
 				$this->ef5_heading_desccription($atts);
-				$this->ef5_heading_button($atts);
+				$this->ef5_heading_button($atts,['class' => 'ef5-btn accent outline ef5-btn-md']);
 			break;
 		}
 	?>
