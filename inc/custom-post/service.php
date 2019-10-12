1ac62ba7c57431a9cdcb78903ad7392bc11d7ca8
vannha <phongnhatk21@gmail.com>
2019-10-12 17:18:40 +0700

    commit -

--- inc/custom-post/service.php
+++ inc/custom-post/service.php
@@ -5,10 +5,9 @@
  * This custom make some custom to Service
  *
  */
-
 add_filter('ef5_extra_post_types', 'overcome_cpts_service', 10 , 1);
 function overcome_cpts_service($post_types) {
-    $post_types['service'] = array( 
+    $post_types['ef5_service'] = array( 
     	'status'        => true,
 		'name'          => esc_html__('OverCome Services', 'overcome'),
 		'singular_name' => esc_html__('OverCome Service', 'overcome'),
@@ -16,7 +15,7 @@ function overcome_cpts_service($post_types) {
 			'menu_position' => 15,
 			'menu_icon'     => 'dashicons-image-filter',
 			'rewrite'       => array(
-				'slug'       => overcome_get_theme_opt('service_slug','overcome'), 
+				'slug'       => overcome_get_theme_opt('service_slug','ef5_service'), 
 				'with_front' => true
             )
         ) 
@@ -28,7 +27,7 @@ add_filter('ef5_extra_taxonomies', 'overcome_cpts_service_tax', 10 , 1);
 function overcome_cpts_service_tax($taxo) {
     $taxo['service_cat'] = array(
     	'status'     => true,
-    	'post_type'  => array('service'), 
+    	'post_type'  => array('ef5_service'), 
         'taxonomy'   => esc_html__('Category', 'overcome'),
         'taxonomies' => esc_html__('Categories', 'overcome'),
         'args'       => array(),
@@ -36,7 +35,7 @@ function overcome_cpts_service_tax($taxo) {
     );
     $taxo['service_tag'] = array(
     	'status'     => true,
-    	'post_type'  => array('service'), 
+    	'post_type'  => array('ef5_service'), 
         'taxonomy'   => esc_html__('Tag', 'overcome'),
         'taxonomies' => esc_html__('Tags', 'overcome'),
         'args'       => array(
