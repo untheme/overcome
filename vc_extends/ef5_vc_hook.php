9902095ced1e9c87b392a621123d9d60892d3e64
vannha <phongnhatk21@gmail.com>
2019-10-12 08:49:27 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -2,7 +2,7 @@
 /**
  * Custom OWL Nav Style
 */
-add_filter('ef5systems_owl_custom_nav_style', 'overcome_owl_custom_nav_style');
+add_filter('ef5systems_carousel_custom_nav_style', 'overcome_owl_custom_nav_style');
 function overcome_owl_custom_nav_style(){
 	return array[
 		esc_html__('Overcome Style 01','overcome') => 'overcome-1'
