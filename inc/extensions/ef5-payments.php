a60c7693c8db1458f4873604e4eb79ec717a47ba
vannha <phongnhatk21@gmail.com>
2019-10-14 16:19:10 +0700

    commit -

---
+++ inc/extensions/ef5-payments.php
@@ -0,0 +1,28 @@
+<?php
+function overcome_loop_donate_info($args = []){
+	$args = wp_parse_args($args,[
+		'layout' => ''
+	]);
+	$post_type = get_post_type(get_the_ID());
+    if(class_exists('EF5Payments') && in_array($post_type, apply_filters('ef5payments_payment_attach_post_types',['ef5_donation']))){
+    	switch ($args['layout']) { 		
+    		default:
+	?>
+		<div class="ef5-loop-donate-info row justify-content-between">
+			<div class="col-md-6">
+				<?php ef5payments_donation_raised(); ?>
+			</div>
+			<div class="col-md-6">
+				<?php 
+					ef5payments_donation_donate_button(); 
+					overcome_post_share(['show_share' => '1','title' => esc_html__('Share:','overcome')]);
+				?>
+			</div>
+		</div>
+	<?php
+			break;
+    	}
+	} else {
+		overcome_post_read_more(['show_readmore' => '1']); 
+	}
+}
