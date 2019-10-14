dce5fb6b51938ed4ea5fa1be60a71cfd5acbfca0
vannha <phongnhatk21@gmail.com>
2019-10-14 15:17:27 +0700

    commit -

--- inc/custom-post/service.php
+++ inc/custom-post/service.php
@@ -7,47 +7,49 @@
  */
 add_filter('ef5_extra_post_type_service', '__return_false');
 
-//add_filter('ef5_extra_post_types', 'overcome_cpts_service', 10 , 1);
+add_filter('ef5_extra_post_types', 'overcome_cpts_service', 10 , 1);
 function overcome_cpts_service($post_types) {
     $supported_service = apply_filters('ef5_extra_post_type_service', false);
-    if(!$supported_service) return;
-    $post_types['ef5_service'] = array( 
-    	'status'        => true,
-		'name'          => esc_html__('OverCome Services', 'overcome'),
-		'singular_name' => esc_html__('OverCome Service', 'overcome'),
-		'args'          => array(
-			'menu_position' => 15,
-			'menu_icon'     => 'dashicons-image-filter',
-			'rewrite'       => array(
-				'slug'       => overcome_get_theme_opt('service_slug','ef5_service'), 
-				'with_front' => true
-            )
-        ) 
-    );
+    if($supported_service) {
+        $post_types['ef5_service'] = array( 
+        	'status'        => true,
+    		'name'          => esc_html__('OverCome Services', 'overcome'),
+    		'singular_name' => esc_html__('OverCome Service', 'overcome'),
+    		'args'          => array(
+    			'menu_position' => 15,
+    			'menu_icon'     => 'dashicons-image-filter',
+    			'rewrite'       => array(
+    				'slug'       => overcome_get_theme_opt('service_slug','ef5_service'), 
+    				'with_front' => true
+                )
+            ) 
+        );
+    }
     return $post_types;
 }
 
-//add_filter('ef5_extra_taxonomies', 'overcome_cpts_service_tax', 10 , 1);
+add_filter('ef5_extra_taxonomies', 'overcome_cpts_service_tax', 10 , 1);
 function overcome_cpts_service_tax($taxo) {
     $supported_service = apply_filters('ef5_extra_post_type_service', false);
-    if(!$supported_service) return;
-    $taxo['service_cat'] = array(
-    	'status'     => true,
-    	'post_type'  => array('ef5_service'), 
-        'taxonomy'   => esc_html__('Category', 'overcome'),
-        'taxonomies' => esc_html__('Categories', 'overcome'),
-        'args'       => array(),
-        'labels'     => array()
-    );
-    $taxo['service_tag'] = array(
-    	'status'     => true,
-    	'post_type'  => array('ef5_service'), 
-        'taxonomy'   => esc_html__('Tag', 'overcome'),
-        'taxonomies' => esc_html__('Tags', 'overcome'),
-        'args'       => array(
-            'hierarchical' => false,
-        ),
-        'labels'     => array()
-    );
+    if($supported_service){
+        $taxo['service_cat'] = array(
+        	'status'     => true,
+        	'post_type'  => array('ef5_service'), 
+            'taxonomy'   => esc_html__('Category', 'overcome'),
+            'taxonomies' => esc_html__('Categories', 'overcome'),
+            'args'       => array(),
+            'labels'     => array()
+        );
+        $taxo['service_tag'] = array(
+        	'status'     => true,
+        	'post_type'  => array('ef5_service'), 
+            'taxonomy'   => esc_html__('Tag', 'overcome'),
+            'taxonomies' => esc_html__('Tags', 'overcome'),
+            'args'       => array(
+                'hierarchical' => false,
+            ),
+            'labels'     => array()
+        );
+    }
     return $taxo;
 }
