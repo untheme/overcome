fadc01dfb416b62f9091ff52fa383736a96ecd56
vannha <phongnhatk21@gmail.com>
2019-10-14 15:34:04 +0700

    commit -

--- inc/custom-post/story.php
+++ inc/custom-post/story.php
@@ -142,7 +142,6 @@ function overcome_story_raised_html($args, $raised){
 // Loop story info
 function overcome_loop_story_info(){
 	$post_type = get_post_type(get_the_ID());
-	var_dump(apply_filters('ef5payments_payment_attach_post_types',[]));
     if(in_array($post_type, apply_filters('ef5payments_payment_attach_post_types',[]))){
 	?>
 		<div class="ef5-loop-story-info row justify-content-between">
