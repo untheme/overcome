3dfd3326455966b7403b50d7ce971cc29a46f7c0
vannha <phongnhatk21@gmail.com>
2019-10-14 10:46:25 +0700

    commit -

--- vc_elements/ef5_counter.php
+++ vc_elements/ef5_counter.php
@@ -676,7 +676,7 @@ class WPBakeryShortCode_ef5_counter extends WPBakeryShortCode{
 		$prefix      = isset($atts['prefix'.$i]) ? $atts['prefix'.$i] : '';
 		$digit_color = isset($atts['digit'.$i.'_color']) ? $atts['digit'.$i.'_color'] : '';
         ?>
-			<div class="<?php echo trim(implode(' ', $css_classes));?>" data-prefix="<?php echo esc_attr($prefix);?>" data-suffix="<?php echo esc_attr($suffix);?>" data-type="<?php echo esc_attr($counter_type);?>" data-digit="<?php echo esc_attr($digit);?>">
+			<div class="<?php echo trim(implode(' ', $css_classes));?>" data-prefix="<?php echo esc_attr($prefix);?>" data-suffix="<?php echo esc_attr($suffix);?>" data-type="<?php echo esc_attr($atts['counter_type']);?>" data-digit="<?php echo esc_attr($digit);?>">
                 <?php if(!empty($prefix)) echo '<span class="prefix">'.overcome_html($prefix).'</span>'; ?>
                 <span class="ef5-counter" <?php if(!empty($digit_color)): ?> style="color:<?php echo esc_attr($digit_color);?>;"<?php endif;?>><?php echo esc_attr($digit); ?></span>
                 <?php if(!empty($suffix)) echo '<span class="suffix">'.overcome_html($suffix).'</span>'; ?>
