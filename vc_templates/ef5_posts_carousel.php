92d622869991aae9a9e0069998bfc435a7da2c2f
vannha <phongnhatk21@gmail.com>
2019-10-15 15:04:47 +0700

    commit -

--- vc_templates/ef5_posts_carousel.php
+++ vc_templates/ef5_posts_carousel.php
@@ -70,6 +70,7 @@
             ?>
             <div class="<?php echo trim(implode(' ',$grid_item_css_class )); ?>" style="animation-delay: <?php echo esc_html($d*100);?>ms">
             <?php
+                var_dump($thumbnail_size_index); var_dump($large_item_class);
                 switch ($layout_template) {
                     case '4':
                 ?>
