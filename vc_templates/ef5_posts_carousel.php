94338be337625b674164542d4fa8c23cb34174a7
vannha <phongnhatk21@gmail.com>
2019-10-15 08:29:42 +0700

    commit -

--- vc_templates/ef5_posts_carousel.php
+++ vc_templates/ef5_posts_carousel.php
@@ -64,8 +64,9 @@
             <?php
                 switch ($layout_template) {
                     case '2':
+                    var_dump(overcome_post_donate_button());
                     $overlay = overcome_post_donate_button(['echo' => false, 'class'=>'ef5-btn ef5-btn-md accent outline']);
-                    var_dump($overlay);
+                    //var_dump($overlay);
             ?>	
             	<div class="<?php echo trim(implode(' ', $item_css_class)); ?> ef5-hover-shadow-1">
                     <?php 
