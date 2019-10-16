a485367f8b7f14705f8ba4f84cc7d660d5c600e9
vannha <phongnhatk21@gmail.com>
2019-10-16 17:08:09 +0700

    commit -

--- vc_templates/ef5_newsletter.php
+++ vc_templates/ef5_newsletter.php
@@ -5,8 +5,8 @@ if(!class_exists('NewsletterWidgetMinimal') && !class_exists('NewsletterWidget')
     $wrapper_class = array('ef5-newsletter', $layout_mode, 'ef5-newsletter-'.$layout_template, $el_class);
 
     $default_form = '[newsletter_form button_label="'.esc_attr($btn_text).'" class="'.esc_attr($el_class).'"]';
-    if($show_name) $default_form .= '[newsletter_field name="name" label="'.esc_attr($name_text).'" placeholder="'.esc_attr($name_text).'"]';
-    $default_form .= '[newsletter_field name="email" label="'.esc_attr($email_text).'" placeholder="'.esc_attr($email_text).'"]';
+    if($show_name) $default_form .= '[newsletter_field name="name" label="" placeholder="'.esc_attr($name_text).'"]';
+    $default_form .= '[newsletter_field name="email" label="" placeholder="'.esc_attr($email_text).'"]';
     $default_form .= '[/newsletter_form]';
 ?>
 <div class="<?php echo trim(implode(' ',$wrapper_class)); ?>">
