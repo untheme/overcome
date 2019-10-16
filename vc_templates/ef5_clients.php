19faa5e7b66a407511adbad3c6db59eaf9e80611
vannha <phongnhatk21@gmail.com>
2019-10-16 08:43:47 +0700

    commit -

--- vc_templates/ef5_clients.php
+++ vc_templates/ef5_clients.php
@@ -19,17 +19,17 @@ extract( $atts );
 $wrap_css_class = ['ef5-clients-wrap ef5-owl-wrap', 'ef5-'.$el_id];
 
 $css_class_attr = $item_class = array();
-$css_class_attr[] = 'ef5-clients-layout-'.$layout_mode;
+$css_class_attr[] = 'ef5-clients-layout-'.$layout_style;
 $css_class_attr[] = 'img-hover-'.$hover_style;
 $item_class[] = 'client-item';
 
-if($layout_mode === 'carousel'){
+if($layout_style === 'carousel'){
     $wrap_css_class[] = '';
     $css_class_attr[] = ef5systems_owl_css_class($atts);
     $item_class[] = 'ef5-carousel-item';
 } else {
     $css_class_attr[] = 'ef5-client-grid row justify-content-center';
-    if($layout_mode === '1') $css_class_attr[] = 'align-items-center';
+    if($layout_style === '1') $css_class_attr[] = 'align-items-center';
 
     $item_class[] = 'ef5-client-grid-item col-'.$col_sm.' col-md-'.$col_md.' col-lg-'.$col_lg.' col-xl-'.$col_xl;
 }
@@ -93,7 +93,7 @@ $item_attrs = '';
                     'class' => 'dot-thumb',
                     'echo'  => false
                 ]);
-                if($layout_mode === 'carousel'){
+                if($layout_style === 'carousel'){
                     $item_attrs = 'data-dot="'.$dot_img.'"';
                 }
                 if($i==1) : ?>
@@ -119,7 +119,7 @@ $item_attrs = '';
             }
         ?>
     </div>
-    <?php if($layout_mode === 'carousel'):
+    <?php if($layout_style === 'carousel'):
         overcome_loading_animation(); 
         ef5systems_owl_dots_container($atts);
         ef5systems_owl_nav_container($atts);
