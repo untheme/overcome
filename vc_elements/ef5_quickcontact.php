f89f7f3af38d3279bddd9d94296bc169826e4073
vannha <phongnhatk21@gmail.com>
2019-10-17 08:44:14 +0700

    commit -

--- vc_elements/ef5_quickcontact.php
+++ vc_elements/ef5_quickcontact.php
@@ -171,6 +171,7 @@ class WPBakeryShortCode_ef5_quickcontact extends WPBakeryShortCode
         $qc_icon    = !empty($iconClass) ? '<span class="'.trim(implode(' ', $icon_classes)).' '.$iconClass.'"></span>' : '';
         $qc_heading = isset($value['qc_heading']) && !empty($value['qc_heading']) ? $value['qc_heading'] : '';
         $qc_text    = isset($value['qc_text']) ? $value['qc_text'] : '';
+        $item_class = ['qc-item'];
         echo '<div class="'.trim(implode(' ', $item_class)).'">';
             echo '<div class="'.trim(implode(' ', $item_inner_class)).'">';
                 if(!empty($iconClass)) echo '<div class="col-auto">'.overcome_html($qc_icon).'</div>';
