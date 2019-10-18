2f8f8cd5aeb8a13fb712a1a5078edad2de04bc35
vannha <phongnhatk21@gmail.com>
2019-10-18 15:20:19 +0700

    commit -

--- inc/functions/header-donate.php
+++ inc/functions/header-donate.php
@@ -1,12 +1,20 @@
 <?php
 function overcome_header_donate_button(){
-	if(!function_exists('ef5payments_donation_donate_button') || overcome_get_opts('header_donate', '0') === '0') return;
-	echo 'xxx';
-	var_dump(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'));
-	var_dump(ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation')));
-	echo ef5payments_donation_donate_button([
-		'id'    => ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation')),
-		'title' => overcome_get_opts('header_donate_label', esc_html__('Donate Now','overcome')),
-		'class' => 'ef5-btn fill accent donate-'.ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'))
-	]);
+	if(!class_exists('EF5Payments') || overcome_get_opts('header_donate', '0') === '0') return;
+    wp_enqueue_script('bootstrap');
+    $post_id = ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'));
+    $data = apply_filters('ef5payments_get_payment_form_data',[
+        'class'        => '',
+        'data-options' => '',
+        'data-target'  => '',
+        'title'	 	   => overcome_get_opts('header_donate_label', esc_html__('Donate Now','overcome')),	
+        'url'		   => '',
+        'target'	   => '_self'	
+    ],$post_id);
+    ?>
+    <a class="<?php echo esc_attr($class) ?>"
+       data-options="<?php echo esc_attr($data['data-options']) ?>"
+       data-target="<?php echo esc_attr($data['data-target']) ?>"
+       href="<?php echo esc_attr($data['url']); ?>" target="<?php echo esc_attr($data['target']); ?>" ><?php echo wp_kses_post($data['title']) ?></a>
+    <?php
 }
