37e05c66b42e2b97ad3c1ceb610e233f78882ea1
vannha <phongnhatk21@gmail.com>
2019-10-18 14:44:29 +0700

    commit -

--- inc/functions/header-donate.php
+++ inc/functions/header-donate.php
@@ -4,7 +4,7 @@ function overcome_header_donate_button(){
 	echo 'xxx';
 	var_dump(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'));
 	var_dump(ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation')));
-	ef5payments_donation_donate_button([
+	echo ef5payments_donation_donate_button([
 		'id'    => ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation')),
 		'title' => overcome_get_opts('header_donate_label', esc_html__('Donate Now','overcome')),
 		'class' => 'ef5-btn fill accent donate-'.ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'))
