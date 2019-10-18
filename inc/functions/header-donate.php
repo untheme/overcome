6a6e74eb19317ea9ae2275d8e2f577284bc00a2c
vannha <phongnhatk21@gmail.com>
2019-10-18 15:30:11 +0700

    commit -

--- inc/functions/header-donate.php
+++ inc/functions/header-donate.php
@@ -5,7 +5,7 @@ function overcome_header_donate_button(){
     //var_dump(overcome_get_opts('header_donate_item',''));
     //var_dump(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'));
     $post_id = ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'));
-    var_dump($post_id);
+    //var_dump($post_id);
     $data = apply_filters('ef5payments_get_payment_form_data',[
         'class'        => '',
         'data-options' => '',
