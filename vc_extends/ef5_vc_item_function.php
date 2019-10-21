91524528807034f71a206283cb927e3c332c9d21
vannha <phongnhatk21@gmail.com>
2019-10-21 14:08:09 +0700

    commit -

--- vc_extends/ef5_vc_item_function.php
+++ vc_extends/ef5_vc_item_function.php
@@ -11,12 +11,10 @@ function overcome_vc_item_meta1($args=[]){
 			<div class="date empty-none col-auto pb-5"><span class="flaticon-calendar ef5-text-accent"></span>&nbsp;&nbsp;'.overcome_posted_date(['echo' => $args['echo']]).'</div>
 			<div class="venue empty-none col-auto pb-5"><span class="flaticon-maps-and-flags ef5-text-accent"></span>&nbsp;&nbsp;'.overcome_posted_by(['echo' => $args['echo']]).'</div>
 			<div class="cost empty-none col-auto"><span class="flaticon-coin-1 ef5-text-accent"></span>&nbsp;&nbsp;'.overcome_posted_in(['echo' => $args['echo']]).'</div>
-		</div>'
+		</div>';
 	if($args['echo']){
 		echo overcome_html($html_output);
 	} else {
 		return $html_output;
 	}
-	?>
-	<?php
 }
