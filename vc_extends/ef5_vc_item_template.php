f6c3cc3e8662fb780b3681fc4947624be4fd1c63
vannha <phongnhatk21@gmail.com>
2019-10-23 16:35:46 +0700

    commit -

--- vc_extends/ef5_vc_item_template.php
+++ vc_extends/ef5_vc_item_template.php
@@ -72,7 +72,7 @@ function overcome_vc_post_layout6($atts, $args = []){
             </div>
             <div class="col-lg-5">
                 <div class="<?php echo trim(implode(' ', $countdown_css_class));?>">
-                    <div class="ef5-countdown-bar ef5-countdown-time" data-count="<?php echo esc_attr(date('Y,m,d,H,i,s', $time)); ?>" data-format="<?php echo esc_attr($time_format);?>" data-label="<?php echo esc_attr($time_label);?>" data-timezone="<?php echo esc_attr($gmt_offset); ?>"></div> 
+                    <div class="ef5-countdown-bar ef5-countdown-time ef5-countdown-layout-1" data-count="<?php echo esc_attr(date('Y,m,d,H,i,s', $time)); ?>" data-format="<?php echo esc_attr($time_format);?>" data-label="<?php echo esc_attr($time_label);?>" data-timezone="<?php echo esc_attr($gmt_offset); ?>"></div> 
                 </div>
             </div>
         </div>
