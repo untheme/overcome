c839db180e66674143e0f54ada2a5a69829de00b
vannha <phongnhatk21@gmail.com>
2019-10-24 11:37:00 +0700

    commit -

--- vc_extends/ef5_vc_item_template.php
+++ vc_extends/ef5_vc_item_template.php
@@ -45,7 +45,7 @@ function overcome_vc_post_layout6($atts, $args = []){
                 $event = get_post( $post );
                 $event_date_format = tribe_get_date_format( true );
                 $time_start = '';
-                $time_end = tribe_get_end_date($event ,true, $event_date_format);
+                $time_end = Tribe__Events__Timezones::event_end_timestamp( get_the_ID() );
                 var_dump('xx'.$time_end);
                 break;
             
