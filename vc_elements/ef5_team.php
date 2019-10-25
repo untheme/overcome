5b7a61e8e39a4b66491fa383b89335606b9bfb98
vannha <phongnhatk21@gmail.com>
2019-10-25 14:02:59 +0700

    commit -

---
+++ vc_elements/ef5_team.php
@@ -0,0 +1,235 @@
+<?php
+vc_map(array(
+    'name'          => 'OverCome Team',
+    'base'          => 'ef5_team',
+    'icon'          => '',
+    'category'      => esc_html__('OverCome', 'overcome'),
+    'description'   => esc_html__('Add your team member', 'overcome'),
+    'params'        => array_merge(
+        array(
+            /* Template Settings */
+            array(
+                'type'          => 'dropdown',
+                'heading'       => esc_html__('Content Alignments','overcome'),
+                'param_name'    => 'content_align',
+                'value'         => array(
+                    'Default'       => '',
+                    'Text Left'     => 'left',
+                    'Text Right'    => 'right',
+                    'Text Center'   => 'center',
+                ),
+                'std'           => '',
+            ),
+            array(
+                'type' => 'img',
+                'heading' => esc_html__('Layout Template','overcome'),
+                'param_name' => 'layout_template',
+                'value' =>  array(
+                    '1' => get_template_directory_uri().'/vc_elements/layouts/team1.png',
+                ),
+                'std' => '1',
+                'admin_label' => true
+            ),
+            array(
+                'type'        => 'el_id',
+                'settings' => array(
+                    'auto_generate' => true,
+                ),
+                'heading'     => esc_html__( 'Element ID', 'overcome' ),
+                'param_name'  => 'el_id',
+                'description' => sprintf( __( 'Enter element ID (Note: make sure it is unique and valid according to <a href="%s" target="_blank">w3c specification</a>).', 'overcome' ), '//w3schools.com/tags/att_global_id.asp' ),
+            ),
+            array(
+                'type'       => 'textfield',
+                'heading'    => esc_html__('Extra Class','overcome'),
+                'param_name' => 'el_class',
+                'value'      => '',
+                'description' => esc_html__('Style particular content element differently - add a class name and refer to it in custom CSS.', 'overcome'),
+            ),
+            /* Members Settings */
+            array(
+                'type'          => 'dropdown',
+                'heading'       => esc_html__('Member image size','overcome'),
+                'param_name'    => 'thumbnail_size',
+                'value'         => unbreak_thumbnail_sizes(),
+                'std'           => 'custom',
+                'group'         => esc_html__('Members','overcome'),
+            ),
+            array(
+                'type'          => 'textfield',
+                'heading'       => esc_html__('Custom member image size','overcome'),
+                'description'   => esc_html__('Alternatively enter size in pixels (Example: 200x100 (Width x Height)).','overcome'),
+                'param_name'    => 'thumbnail_size_custom',
+                'value'         => '',
+                'std'           => '',
+                'group'         => esc_html__('Members','overcome'),
+                'dependency'    => array(
+                    'element'   => 'thumbnail_size',
+                    'value'     => 'custom',
+                ),
+            ),
+            array(
+                'type'          => 'checkbox',
+                'param_name'    => 'thumbnail_bw',
+                'value'         => array(
+                    esc_html__('Make Member image as Black & White','overcome') => '1'
+                ),
+                'std'           => '0',
+                'group'         => esc_html__('Members', 'overcome')
+            ),
+            array(
+                'type'          => 'param_group',
+                'heading'       => esc_html__( 'Add Member', 'overcome' ),
+                'param_name'    => 'teams',
+                'std'           => urlencode( json_encode( array(
+                    array(
+                        'name'     => 'Roberto Antonio',
+                        'position' => 'Director',
+                        'link'     => 'title:Roberto Antonio||url:#',
+                    ),
+                    array(
+                        'name'     => 'Damien Ruth',
+                        'position' => 'Ceo & Architect',
+                        'link'     => 'title:Damien Ruth||url:#',
+                    ),
+                    array(
+                        'name'     => 'Ashley Fletcher',
+                        'position' => 'Director',
+                        'link'     => 'title:Ashley Fletcher||url:#',
+                    ),
+                    array(
+                        'name'     => 'Marie Mercury',
+                        'position' => 'Elevator',
+                        'link'     => 'title:Marie Mercury||url:#',
+                    ),
+                    array(
+                        'name'     => 'William',
+                        'position' => 'Founder & CEO',
+                        'desc'     => '',
+                        'link'     => 'title:William||url:#',
+                    ),
+                    array(
+                        'name'     => 'Charlotte',
+                        'position' => 'Marketing',
+                        'desc'     => '',
+                        'link'     => 'title:Charlotte||url:#',
+                    ),
+                    array(
+                        'name'     => 'Benjamin',
+                        'position' => 'Graphic Designer',
+                        'desc'     => '',
+                        'link'     => 'title:Benjamin||url:#',
+                    ),
+                    array(
+                        'name'     => 'Amelia',
+                        'position' => 'Support',
+                        'desc'     => '',
+                        'link'     => 'title:Amelia||url:#',
+                    ),
+                ) ) ),
+                'params'        => array(
+                    array(
+                        'type'          => 'attach_image',
+                        'heading'       => esc_html__( 'Image', 'overcome' ),
+                        'param_name'    => 'image',
+                    ),
+                    array(
+                        'type'          => 'textfield',
+                        'heading'       => esc_html__( 'Name', 'overcome' ),
+                        'param_name'    => 'name',
+                        'admin_label'   => true,
+                    ),
+                    array(
+                        'type'          => 'textfield',
+                        'heading'       => esc_html__( 'Position', 'overcome' ),
+                        'param_name'    => 'position',
+                    ),
+                    array(
+                        'type'          => 'textarea',
+                        'heading'       => esc_html__( 'Description', 'overcome' ),
+                        'param_name'    => 'desc',
+                    ),
+                    array(
+                        'type'          => 'vc_link',
+                        'heading'       => esc_html__( 'Member Page', 'overcome' ),
+                        'param_name'    => 'link',
+                        'description'   => esc_html__( 'Enter link to member details page.', 'overcome' ),
+                    ),
+                    array(
+                        'type'          => 'param_group',
+                        'heading'       => esc_html__( 'Member Social', 'overcome' ),
+                        'param_name'    => 'social',
+                        'std'           => urlencode( json_encode( array(
+                            array(
+                                'icon' => 'fa fa-facebook',
+                                'link' => 'title:Facebook||url:facebook.com'
+                            ),
+                            array(
+                                'icon' => 'fa fa-twitter',
+                                'link' => 'title:Twitter||url:twitter.com'
+                            ),
+                            array(
+                                'icon' => 'fa fa-google-plus',
+                                'link' => 'title:Google Plus||url:plus.google.com'
+                            ),
+                            array(
+                                'icon' => 'fa fa-linkedin',
+                                'link' => 'title:Linkedin||url:linkedin.com'
+                            ),
+                        ) ) ),
+                        'params'        => array(
+                            array(
+                                'type'          => 'iconpicker',
+                                'heading'       => esc_html__( 'Social icon', 'overcome' ),
+                                'param_name'    => 'icon',
+                                'admin_label'   => true,
+                            ),
+                            array(
+                                'type'          => 'vc_link',
+                                'heading'       => esc_html__( 'Enter social link', 'overcome' ),
+                                'param_name'    => 'link',
+                            ),
+                        ),
+                    ),
+                    
+                ),
+                'group'     => esc_html__('Members','overcome')
+            ),
+            array(
+                'type'       => 'dropdown',
+                'heading'    => esc_html__('Layout Style','overcome'),
+                'param_name' => 'layout_style',
+                'value'      =>  array(
+                    esc_html__('Grid','overcome')     => 'grid',
+                    esc_html__('Carousel','overcome') => 'carousel'
+                ),
+                'std'        => 'grid',
+                'group'      => esc_html__('Layout Settings','overcome'),
+                'admin_label' => true
+            )
+        ),
+        /* Grid settings */
+        ef5systems_grid_settings(array(
+                'group'                  => esc_html__('Layout Settings','overcome'), 
+                'dependency_element'     => 'layout_style', 
+                'dependency_value_value' => 'grid'
+            )
+        ),
+        /* Carousel Settings */
+        ef5systems_owl_settings(array(
+                'group'      => esc_html__('Layout Settings','overcome'), 
+                'param_name' => 'layout_style', 
+                'value'      => 'carousel'
+            )
+        )
+    )
+));
+
+class WPBakeryShortCode_ef5_team extends WPBakeryShortCode
+{
+    protected function content($atts, $content = null){
+        $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+        ef5systems_owl_call_settings($atts);
+        return parent::content($atts, $content);
+    }
+}
---
+++ vc_templates/ef5_team.php
@@ -0,0 +1,184 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    die( '-1' );
+}
+/**
+ * Shortcode attributes
+ * @var $atts
+ * Shortcode class
+ * @var $this WPBakeryShortCode_cms_team
+ */
+$atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+extract( $atts );
+$wrap_css_class = ['ef5-team-wrap'];
+$css_class_attr = $item_class = array();
+$css_class_attr[] = 'ef5-team ef5-team-layout-'.$layout_template;
+$item_class[] = 'team-item';
+
+if($layout_style === 'carousel'){
+	$wrap_css_class[] = ef5_owl_css_class($atts);
+	$css_class_attr[] = 'ef5-owl team-carousel owl-carousel';
+	$item_class[]     = 'ef5-carousel-item';
+} else {
+	$css_class_attr[] = 'ef5-grid row align-items-center justify-content-center';
+	$item_class[]     = 'ef5-grid-item col-'.$col_sm.' col-md-'.$col_md.' col-lg-'.$col_lg.' col-xl-'.$col_xl;
+}
+$text_align = '';
+if(!empty($content_align)) {
+    $text_align = 'text-'.$content_align;
+} else {
+    if(in_array($layout_template, ['2','3'])) {
+        $text_align = 'text-center';
+        $content_align = 'center';
+    }
+}
+$css_class_attr[] = $text_align;
+$css_class_attr[] = $el_class;
+
+/* get space for owl item */
+$owl_item_space = '';
+if(isset($margin) && (isset($number_row) && $number_row > 1 )){
+    $owl_item_space = 'style="margin-bottom:'.$margin.'px"';
+}
+// Inner class 
+$item_inner_class = ['inner-box'];
+
+// get testinomial data
+$teams = (array) vc_param_group_parse_atts( $atts['teams'] );
+// Team Image Size
+if( $thumbnail_size === 'custom')
+	$thumbnail_size = $thumbnail_size_custom;
+if(empty($thumbnail_size)){
+    switch ($layout_template) {
+        case '5':
+            $thumbnail_size = '240';
+            break;
+        case '4':
+            $thumbnail_size = '270';
+            break;
+        case '3':
+            $thumbnail_size = '370x460';
+            break;
+        case '2':
+            $thumbnail_size = '270x350';
+            break;
+        default:
+            $thumbnail_size = '270';
+            break;
+    }
+}
+// OWL Dots thumbnail Size
+switch ($layout_template) {
+    case '4': 
+        $item_inner_class[] = 'shadow-hover'; 
+        break;
+	default:
+		$dot_thumbnail_size = '80';
+		break;
+}
+$count = count($teams);
+$i=1;
+$j=0;
+$team_link_open = $team_link_close = $socials = '';
+?>
+<div class="<?php echo trim(implode(' ', $wrap_css_class));?>">
+    <div id="<?php echo esc_attr($el_id);?>" class="<?php echo trim(implode(' ',$css_class_attr));?>"> 
+        <?php
+            foreach($teams as $team){
+                $team['image']  = isset($team['image']) ? $team['image'] : null;
+                $j++;
+                if($i > $number_row) $i=1;
+                
+                if(isset($team['name'])) {
+                	if(!empty($team['link'])){
+					    $team_link = vc_build_link( $team['link']);
+					    $team_link = ( $team_link == '||' ) ? '' : $team_link;  
+					    if ( strlen( $team_link['url'] ) > 0 ) {
+					        $a_href = $team_link['url'];
+					        $a_title = !empty($team_link['title']) ? $team_link['title'] : $team['name'];
+					        $a_target = strlen( $team_link['target'] ) > 0 ? $team_link['target'] : '_blank';
+					    }
+					    $team_link_open = '<a href="'.esc_url($a_href).'" title="'.esc_attr($a_title).'" target="'.esc_attr($a_target).'">';
+                		$team_link_close = '</a>';
+					}
+					if(!empty($team['social'])){
+						$socials_list = '';
+						$socials = (array) vc_param_group_parse_atts( $team['social']);
+                        foreach($socials as $social){
+                            if(isset($social['icon'])) $icon = '<span class="'.$social['icon'].'"></span>';
+                            /* parse social link */
+                            $link = false;
+                            if(isset($social['link'])){
+                                $icon_link = vc_build_link( $social['link'] );
+                                $icon_link = ( $icon_link == '||' ) ? '' : $icon_link;
+                                if ( strlen( $icon_link['url'] ) > 0 ) {
+                                    $link = true;
+                                    $social_href = $icon_link['url'];
+                                    $social_title = $icon_link['title'] ? $icon_link['title'] : '';
+                                    $social_target = strlen( $icon_link['target'] ) > 0 ? str_replace(' ','',$icon_link['target']) : '_self';
+                                }
+                            }
+                            if($link){
+                                $link_open = '<a href="'.esc_url($social_href).'" title="'.esc_attr($social_title).'" target="'.esc_attr($social_target).'">';
+                                $link_close = '</a>';
+                                $socials_list .= $link_open.$icon.$link_close;
+                            }     
+                        }
+					}
+                	// dot image
+                	if(!empty($team['image']))
+                    	$dot_image = overcome_image_by_size(['id'=>$team['image'], 'size'=>$dot_thumbnail_size, 'class'=>'dot-thumb circle', 'echo' => false]);
+                    else 
+                    	$dot_image = overcome_default_image_thumbnail(['size' => $dot_thumbnail_size, 'class' => 'dot-thumb circle']);
+                    if($i==1) : ?>
+                        <div class="<?php echo join(' ',$item_class);?>" data-dot='<?php echo wp_kses_post($dot_image); ?>'>
+                    <?php  
+                        endif;
+                        echo '<div class="ef5-item-inner" '.$owl_item_space.'><div class="'.implode(' ', $item_inner_class).'">';
+                        	switch ($layout_template) {
+                        		default:
+                            ?>
+                                <div class="hoverdir-wrap fade-in">
+                                    <div class="hover-inner team-images">
+                                        <?php
+                                            // image            
+                                            overcome_image_by_size([
+                                                'id'      => $team['image'],
+                                                'size'    => $thumbnail_size,
+                                                'class'   => 'team-img transition',
+                                                'default' => true
+                                            ]);
+                                		?>
+                                        <div class="hover-content content-flex align-items-end justify-content-<?php echo esc_attr($content_align);?> overlay-bg-1">
+                                            <div class="team-social ef5-social text-white size-30"><?php echo overcome_html($socials_list);?></div>
+                                        </div>
+                                    </div>
+                            		<div class="team-info">
+    	                        		<?php
+    	                        			// name
+    			                            echo '<div class="team-name h4 text-truncate">'.$team_link_open.$team['name'].$team_link_close.'</div>';
+    			                            // position
+    			                            echo '<div class="team-position text-truncate">'.$team['position'].'</div>';
+    	                        			// desc 
+    			                        	if(!empty($team['desc'])) echo '<div class="team-desc">'.$team['desc'].'</div>';
+    			                        	
+    			                        ?>
+    			                    </div>
+                                </div>
+			                    <?php
+                        		break;
+                        	}
+                        echo '</div></div>';
+                    if($i == $number_row || $j==$count) echo '</div>';
+                    $i ++;
+                }
+            }
+        ?>
+    </div>
+    <?php if($layout_style === 'carousel'):
+        overcome_loading_animation(); 
+        ef5systems_owl_dots_container($atts);
+        ef5systems_owl_nav_container($atts);
+        ef5systems_owl_dots_in_nav_container($atts);
+    endif; ?>
+</div>
