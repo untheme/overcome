15a7a916570e804532cb46f61de21089155b213c
vannha <phongnhatk21@gmail.com>
2019-10-26 16:31:22 +0700

    commit -

--- vc_elements/ef5_heading.php
+++ vc_elements/ef5_heading.php
@@ -241,7 +241,7 @@ class WPBakeryShortCode_ef5_heading extends WPBakeryShortCode
     }
     protected function ef5_heading_main_heading_icon($atts, $args=[]){
         extract($atts);
-        if(empty($atts['heading_text']) || $atts['add_heading_icon'] === 'false') return;
+        if(empty($atts['heading_text']) || $atts['add_heading_icon'] !== '3' || $atts['add_heading_icon'] === 'false') return;
         $args = wp_parse_args($args,[
             'class' => ''
         ]);
