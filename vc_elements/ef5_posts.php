85955cdada4b24eb4be639702f7c2f23c88e7c32
vannha <phongnhatk21@gmail.com>
2019-10-26 09:12:20 +0700

    commit -

--- vc_elements/ef5_posts.php
+++ vc_elements/ef5_posts.php
@@ -227,21 +227,23 @@ class WPBakeryShortCode_ef5_posts extends WPBakeryShortCode
             'thumbnail_size' => '570',
         ]);
 
-        $overlay_content = '<div class="overlay ef5-bg-overlay ef5-rounded-10"><div class="overlay-inner center-align w-100 p-30 pl-lg-75 pr-lg-75">';
-        $overlay_content .= overcome_post_title(['echo' => false, 'class'=>'text-22 text-md-36 text-lg-22 text-xl-36 lh-xl-46 text-white pb-15 pb-xl-58']);
-        $overlay_content .= overcome_post_excerpt([
-            'show_excerpt' => '1', 
-            'length'       => '15', 
-            'more'         => '',
-            'class'        => 'text-white d-none d-md-block',
-            'echo'         => false 
-        ]);
-        $overlay_content .= overcome_tribe_events_info(['echo' => false, 'class'=>'text-white mb-13 pb-15 pb-lg-45']);
-        $overlay_content .= get_post_type() === 'tribe_events' ?  overcome_post_read_more(['show_readmore' => '1', 'echo' => false, 'title' => esc_html__('View Event','overcome'),'class'=>'ef5-btn ef5-btn-md accent outline']) :  overcome_post_read_more(['show_readmore' => '1', 'echo' => false,'class'=>'ef5-btn ef5-btn-md accent outline']);
+        ob_start();
+            $overlay_content = '<div class="overlay ef5-bg-overlay ef5-rounded-10"><div class="overlay-inner center-align w-100 p-30 pl-lg-75 pr-lg-75">';
+            $overlay_content .= overcome_post_title(['echo' => false, 'class'=>'text-22 text-md-36 text-lg-22 text-xl-36 lh-xl-46 text-white pb-15 pb-xl-58']);
+            $overlay_content .= overcome_post_excerpt([
+                'show_excerpt' => '1', 
+                'length'       => '15', 
+                'more'         => '',
+                'class'        => 'text-white d-none d-md-block',
+                'echo'         => false 
+            ]);
+            $overlay_content .= overcome_tribe_events_info(['echo' => false, 'class'=>'text-white mb-13 pb-15 pb-lg-45']);
+            $overlay_content .= get_post_type() === 'tribe_events' ?  overcome_post_read_more(['show_readmore' => '1', 'echo' => false, 'title' => esc_html__('View Event','overcome'),'class'=>'ef5-btn ef5-btn-md accent outline']) :  overcome_post_read_more(['show_readmore' => '1', 'echo' => false,'class'=>'ef5-btn ef5-btn-md accent outline']);
 
-        $overlay_content .= overcome_loop_donate_info(); 
+            $overlay_content .= overcome_loop_donate_info(); 
 
-        $overlay_content .= '</div></div>';
+            $overlay_content .= '</div></div>';
+        $overlay_content = ob_get_clean();
 
         $css_class = ['ef5-post-item-featured','col-lg-6', $args['class'],'mb-30 mb-lg-0'];
         ?>
