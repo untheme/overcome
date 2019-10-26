5fc94d359da628aef6d8cab8a0a8be141012b99c
vannha <phongnhatk21@gmail.com>
2019-10-26 10:32:22 +0700

    commit -

--- vc_elements/ef5_posts.php
+++ vc_elements/ef5_posts.php
@@ -390,6 +390,7 @@ class WPBakeryShortCode_ef5_posts extends WPBakeryShortCode
                             'class'        => 'text-13 mb-17' 
                         ]);
                         overcome_tribe_events_info_hori(['class' => 'text-13']);
+                        overcome_loop_donate_info(['layout' => '3']);
                     ?>
                 </div>
             </div>
