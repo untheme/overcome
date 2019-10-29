b1d4956ff411493fc855084fdc7a5779dd1d47fd
vannha <phongnhatk21@gmail.com>
2019-10-29 15:23:34 +0700

    commit -

--- vc_elements/ef5_heading.php
+++ vc_elements/ef5_heading.php
@@ -88,6 +88,8 @@ vc_map(array(
             ef5systems_vc_text_style([
                 'color'     => 'text_color_opts',
                 'element'   => 'heading_text',
+                'value'     => 'not_empty',
+                'value_opt' => true,
                 'group'     => esc_html__('Heading','overcome')
             ]),
             array(
