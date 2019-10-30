216e600637fa3bbca4d41948fe99f8a25e905233
vannha <phongnhatk21@gmail.com>
2019-10-30 14:00:25 +0700

    commit -

--- vc_templates/ef5_fancy_box.php
+++ vc_templates/ef5_fancy_box.php
@@ -16,6 +16,24 @@ $wrap_css_class = ['ef5-fancybox','ef5-fancybox-'.$layout_template, 'transition'
 ?>
 <div class="<?php echo trim(implode(' ', $wrap_css_class));?>">
 	<?php switch ($layout_template) {
+		case '3':
+		?>
+			<div class="row gutter-40">
+				<div class="col-auto">
+					<?php
+					echo overcome_html($this->ef5_fancy_box_icon($atts,['class' => 'text-50 ef5-text-accent']));
+					?>
+				</div>
+				<div class="col">
+					<?php
+			        	echo overcome_html($this->ef5_fancy_box_heading($atts,['class'=> 'text-18 lh-1/33333333']));
+			        	echo overcome_html($this->ef5_fancy_box_desc($atts));
+			        	echo overcome_html($this->ef5_fancy_box_link($atts));
+			        ?>
+			    </div>
+        	</div>
+        <?php
+			break;
 		default:
         	echo overcome_html($this->ef5_fancy_box_icon($atts,['class' => 'text-56 ef5-text-accent']));
         	echo overcome_html($this->ef5_fancy_box_heading($atts,['class'=> 'text-18 lh-1/33333333']));
