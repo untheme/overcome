f5e807e07b4699a69024f8a6f8644f33f125c526
vannha <phongnhatk21@gmail.com>
2019-10-30 11:08:54 +0700

    commit -

--- vc_templates/ef5_heading.php
+++ vc_templates/ef5_heading.php
@@ -23,9 +23,14 @@ extract( $atts );
 				$this->ef5_heading_button($atts,['class' => 'ef5-btn accent outline ef5-btn-md']);
 				break;
 			default:
+				$text_size = ef5systems_get_vc_param_value($atts, 'ef5_size');
+        		$custom_text_size = ef5systems_get_vc_param_value($atts, 'ef5_size', true);
+        		$ef5_heading_main_heading_class = 'text-36 font-style-700';
+        		if($text_size === 'custom' && !empty($custom_text_size)) $ef5_heading_main_heading_class = '';
+
 				$this->ef5_heading_small_heading($atts, ['class' => 'text-22 text-uppercase ef5-text-accent']);
 				$this->ef5_heading_main_heading_icon($atts);
-				$this->ef5_heading_main_heading($atts,['class' => 'text-36 font-style-700 ef5-heading']);
+				$this->ef5_heading_main_heading($atts,['class' => 'ef5-heading '.$ef5_heading_main_heading_class]);
 				$this->ef5_heading_sub_heading($atts, ['class' => 'text-22']);
 				$this->ef5_heading_desccription($atts);
 				$this->ef5_heading_button($atts,['class' => 'ef5-btn accent fill ef5-btn-md']);
