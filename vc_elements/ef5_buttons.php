ccb15a603339787786cc26958fb75d0415feac8f
vannha <phongnhatk21@gmail.com>
2019-10-31 10:56:03 +0700

    commit -

---
+++ vc_elements/ef5_buttons.php
@@ -0,0 +1,235 @@
+<?php
+vc_map(array(
+    'name'          => 'OverCome Button',
+    'base'          => 'ef5_button',
+    'category'      => esc_html__('OverCome', 'overcome'),
+    'description'   => esc_html__('OverCome button style', 'overcome'),
+    'icon'         => 'icon-wpb-ui-button',
+    'params'        => array_merge(
+        array(
+            array(
+                'type'          => 'textfield',
+                'param_name'    => 'btn_text',
+                'heading'       => esc_html__( 'Button Text', 'overcome' ),
+                'value'         => 'Button',
+                'std'           => 'Button',
+                'admin_label'   => true
+            ),
+            array(
+                'type'          => 'vc_link',
+                'heading'       => esc_html__('Button link','overcome'),
+                'param_name'    => 'button_link',
+                'value'         => '',
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_display',
+                'heading'       => esc_html__( 'Button Display', 'overcome' ),
+                'value'         => array(
+                    esc_html__( 'List', 'overcome' )  => 'list', 
+                    esc_html__( 'Block', 'overcome' ) => 'block', 
+                ),
+                'std'           => 'block',
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_align',
+                'heading'       => esc_html__( 'Button Alignment', 'overcome' ),
+                'value'         => array(
+                    esc_html__( 'Default', 'overcome' ) => '',
+                    esc_html__( 'Start', 'overcome' )   => 'start', 
+                    esc_html__( 'End', 'overcome' )     => 'end',
+                    esc_html__( 'Center', 'overcome' )  => 'center',
+                ),
+                'std'           => '',
+                'dependency'    => array(
+                    'element'   => 'btn_display',
+                    'value'     => 'block',
+                ),
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_style',
+                'heading'       => esc_html__( 'Button Style', 'overcome' ),
+                'value'         => overcome_button_style(),
+                'std'           => 'fill',
+                'admin_label'   => true,
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_color',
+                'heading'       => esc_html__( 'Button Color', 'overcome' ),
+                'value'         => overcome_button_colors(),
+                'std'           => 'primary',
+                'admin_label'   => true,
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+            array(
+                'type'          => 'colorpicker',
+                'param_name'    => 'btn_custom_bg_color',
+                'heading'       => esc_html__( 'Background/Border Color', 'overcome' ),
+                'value'         => '',
+                'std'           => '',
+                'edit_field_class' => 'vc_col-sm-6',
+                'dependency'    => array(
+                    'element'   => 'btn_color',
+                    'value'     => 'custom',
+                ),
+            ),
+            array(
+                'type'          => 'colorpicker',
+                'param_name'    => 'btn_custom_text_color',
+                'heading'       => esc_html__( 'Text Color', 'overcome' ),
+                'value'         => '',
+                'std'           => '',
+                'edit_field_class' => 'vc_col-sm-6',
+                'dependency'    => array(
+                    'element'   => 'btn_color',
+                    'value'     => 'custom',
+                ),
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_shape',
+                'heading'       => esc_html__( 'Button Shape', 'overcome' ),
+                'value'         => overcome_button_shapes(),
+                'std'           => '',
+                'admin_label'   => true,
+                'edit_field_class' => 'vc_col-sm-6',
+                'dependency'    => array(
+                    'element'            => 'btn_style',
+                    'value_not_equal_to' => 'simple',
+                ),
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_size',
+                'heading'       => esc_html__( 'Button Size', 'overcome' ),
+                'value'         => overcome_button_size(),
+                'std'           => 'df',
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_underline',
+                'heading'       => esc_html__( 'Add Underline', 'overcome' ),
+                'value'         => array(
+                    esc_html__( 'None', 'overcome' )      => '',
+                    esc_html__( 'Default', 'overcome' )   => 'underline-default',
+                    esc_html__( 'Primary', 'overcome' )   => 'underline-primary',
+                    esc_html__( 'Accent', 'overcome' )    => 'underline-accent',
+                    esc_html__( 'Secondary', 'overcome' ) => 'underline-secondary',
+                ),
+                'std'           => '',
+                'dependency'    => array(
+                    'element'   => 'btn_style',
+                    'value'     => 'simple',
+                ),
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_hover_style',
+                'heading'       => esc_html__( 'Hover Style', 'overcome' ),
+                'value'         => array(
+                    esc_html__('Default', 'overcome') => '',
+                    //esc_html__('Slide', 'overcome')   => 'ef5-btn-slide',
+                    //esc_html__('3D', 'overcome')      => 'ef5-btn-3d',
+                ),
+                'std'              => '',
+                'edit_field_class' => 'vc_col-sm-6',
+                'dependency'       => array(
+                    'element'            => 'btn_style',
+                    'value_not_equal_to' => 'simple',
+                ),
+            ),
+            array(
+                'type'          => 'checkbox',
+                'param_name'    => 'add_icon',
+                'heading'       => esc_html__( 'Add Icon?', 'overcome' ),
+                'std'           => false,
+                'group'         => esc_html__('Icon','overcome')
+            ),
+        ),
+        ef5systems_icon_libs(),
+        ef5systems_icon_libs_icon(),
+        array(
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'icon_position',
+                'heading'       => esc_html__( 'Icon Position', 'overcome' ),
+                'value'         => array(
+                    esc_html__( 'Left', 'overcome' )     => 'icon-left',
+                    esc_html__( 'Right', 'overcome' )    => 'icon-right',
+                ),
+                'std'           => 'icon-right',
+                'dependency'    => array(
+                    'element'   => 'add_icon',
+                    'value'     => 'true',
+                ),
+                'group'         => esc_html__('Icon','overcome'),
+                'edit_field_class' => 'vc_col-sm-4'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_icon_style',
+                'heading'       => esc_html__( 'Icon Style', 'overcome' ),
+                'value'         => array(
+                    esc_html__( 'Default', 'overcome' ) => '',
+                ),
+                'std'           => '',
+                'dependency'    => array(
+                    'element'   => 'add_icon',
+                    'value'     => 'true',
+                ),
+                'group'         => esc_html__('Icon','overcome'),
+                'edit_field_class' => 'vc_col-sm-4'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_icon_color',
+                'heading'       => esc_html__( 'Icon Color', 'overcome' ),
+                'value'         => array(
+                    esc_html__( 'Default', 'overcome' )   => '',
+                    esc_html__( 'Primary', 'overcome' )   => 'primary',
+                    esc_html__( 'Accent', 'overcome' )    => 'accent',
+                    esc_html__( 'Secondary', 'overcome' ) => 'secondary',
+                ),
+                'std'           => '',
+                'dependency'    => array(
+                    'element'   => 'add_icon',
+                    'value'     => 'true',
+                ),
+                'group'         => esc_html__('Icon','overcome'),
+                'edit_field_class' => 'vc_col-sm-4'
+            ),
+            array(
+                'type'          => 'dropdown',
+                'param_name'    => 'btn_icon_animation',
+                'heading'       => esc_html__( 'Icon Animation', 'overcome' ),
+                'value'         => array(
+                    esc_html__( 'None', 'overcome' )        => '',
+                    esc_html__( 'Default', 'overcome' )     => 'anim-default'
+                ),
+                'std'           => '',
+                'dependency'    => array(
+                    'element'   => 'add_icon',
+                    'value'     => 'true',
+                ),
+                'group'         => esc_html__('Icon','overcome'),
+                'edit_field_class' => 'vc_col-sm-6'
+            ),
+        )
+    )
+));
+
+class WPBakeryShortCode_ef5_button extends WPBakeryShortCode
+{
+    protected function content($atts, $content = null)
+    {
+        return parent::content($atts, $content);
+    }
+}
