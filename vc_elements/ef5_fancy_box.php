eb3ae4bcac454a44db3a95df8c60e938fe08d301
vannha <phongnhatk21@gmail.com>
2019-11-21 11:23:43 +0700

    commit -

--- vc_elements/ef5_fancy_box.php
+++ vc_elements/ef5_fancy_box.php
@@ -14,6 +14,7 @@ vc_map(array(
                 'value'      =>  array(
                     '1' => get_template_directory_uri().'/vc_elements/layouts/fancy-box1.png',
                     '3' => get_template_directory_uri().'/vc_elements/layouts/fancy-box3.png',
+                    '4' => get_template_directory_uri().'/vc_elements/layouts/fancy-box4.png',
                 ),
                 'std'        => '1',
                 'admin_label' => true
