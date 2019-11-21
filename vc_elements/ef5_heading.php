81fac3d1710312029566c9b5b0afb96fc1507097
vannha <phongnhatk21@gmail.com>
2019-11-21 16:16:54 +0700

    commit -

--- vc_elements/ef5_heading.php
+++ vc_elements/ef5_heading.php
@@ -338,7 +338,7 @@ class WPBakeryShortCode_ef5_heading extends WPBakeryShortCode
         $text_color = isset($text_color) && !empty($text_color) ? $text_color : 'default';
         $text_size = isset($text_size) && !empty($text_size) ? 'text-'.$text_size : '';
         $font_style = isset($font_style) && !empty($font_style) ? 'font-style-'.$font_style : '';
-        $line_height = isset($line_height) && !empty($line_height) ? 'lh-'.$line_height, : '';
+        $line_height = isset($line_height) && !empty($line_height) ? 'lh-'.$line_height : '';
         $heading_css_class = [
             'main-heading',
             'ef5-text-'.$text_color,
