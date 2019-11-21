2fc0fc480f0ff8e9114b3cbb9a73565e7948c3ce
vannha <phongnhatk21@gmail.com>
2019-11-21 17:02:30 +0700

    commit -

--- vc_templates/ef5_posts.php
+++ vc_templates/ef5_posts.php
@@ -149,7 +149,7 @@
                     }
                     $overlay = overcome_post_donate_button(['echo' => false, 'class'=>'ef5-btn ef5-btn-md accent outline']);
                 ?>  <div class="<?php echo trim(implode(' ',$grid_item_css_class )); ?>" style="animation-delay: <?php echo esc_html($d*100);?>ms">
-                        <div class="<?php echo trim(implode(' ', $item_css_class)); ?> overlay-wrap ef5-hover-shadow-1">
+                        <div class="<?php echo overcome_optimize_css_class(implode(' ', $item_css_class)); ?> overlay-wrap ef5-hover-shadow-1">
                             <?php 
                                 overcome_post_media([
                                     'thumbnail_size' => $thumbnail_size[$thumbnail_size_index], 
