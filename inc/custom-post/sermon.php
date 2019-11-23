f4db948f871d2fa846ab4b2369f6bed3df33dc2b
vannha <phongnhatk21@gmail.com>
2019-11-23 10:33:21 +0700

    commit -

--- inc/custom-post/sermon.php
+++ inc/custom-post/sermon.php
@@ -93,8 +93,8 @@ function overcome_sermon_icons($args = []){
     <div class="sermon-icon">
     	<a href="#sermon-video-<?php the_ID();?>" class="mfp-inline"><?php echo overcome_html($args['icon_video']); ?>video</a>
     	<a href="#sermon-audio-<?php the_ID();?>" class="mfp-inline"><?php echo overcome_html($args['icon_audio']); ?>audio</a>
-    	<a href="<?php echo esc_url($sermon_download['url']);?>"><?php echo overcome_html($args['icon_file']); ?>download</a>
-    	<a href="<?php echo esc_url($sermon_pdf['url']);?>"><?php echo overcome_html($args['icon_docs']); ?>pdf</a>
+    	<a href="<?php echo esc_url($sermon_download['url']);?>" download><?php echo overcome_html($args['icon_file']); ?>download</a>
+    	<a href="<?php echo esc_url($sermon_pdf['url']);?>" download><?php echo overcome_html($args['icon_docs']); ?>pdf</a>
     	<div class="d-none">
 	    	<?php 
 	    		sermon_popup_video();
