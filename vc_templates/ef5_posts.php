66b4fc2c8163df8cd292e34502737e5aac2b5592
vannha <phongnhatk21@gmail.com>
2019-11-25 15:36:32 +0700

    commit -

--- vc_templates/ef5_posts.php
+++ vc_templates/ef5_posts.php
@@ -319,13 +319,13 @@
                                 overcome_post_media([
                                     'thumbnail_size' => overcome_default_value($atts['thumbnail_size'], '370x240'), 
                                     'default_thumb'  => true,
-                                    'class'          => 'col-md-4 col-lg-400',
+                                    'class'          => 'col-md-4 col-lg-35/8333333',
                                     'before'         => '<a href="'.get_the_permalink().'">',
                                     'after'          => '</a>',
                                     'img_class'      => 'ef5-rounded-5'
                                 ]); 
                             ?>
-                            <div class="col-md-8 col-lg-auto">
+                            <div class="col-md-8 col-lg-64/1666667">
                                 <?php
                                     overcome_post_title([
                                         'heading_tag' => 'text-28 lh-32 font-style-500'
