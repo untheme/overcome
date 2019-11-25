3723a92ec5f74bb544ba2949e63b06aeb1218371
vannha <phongnhatk21@gmail.com>
2019-11-25 08:41:49 +0700

    commit -

--- vc_templates/ef5_posts.php
+++ vc_templates/ef5_posts.php
@@ -338,13 +338,7 @@
                                     ]);
                                     
                                     if(class_exists('EF5Payments')) {
-                                        ef5systems_donation_progress_donors([
-                                            'donor_icon' => '<span class="flaticon-like"></span>'
-                                        ]);
-                                        ef5payments_donation_donate_amount([
-                                            'goal_label' => esc_html__('Goal:','overcome'),
-                                            'raised_label' => esc_html__('Raised:','overcome')
-                                        ]);
+                                        ef5systems_donation_layout_2();
                                     }
 
                                     overcome_sermon_icons([
