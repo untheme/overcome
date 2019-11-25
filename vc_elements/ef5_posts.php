634c2f1e340de47e6eb12874c6f6274f2cd0a52b
vannha <phongnhatk21@gmail.com>
2019-11-25 16:25:25 +0700

    commit -

--- vc_elements/ef5_posts.php
+++ vc_elements/ef5_posts.php
@@ -89,6 +89,7 @@ vc_map(array(
                 'param_name' => 'layout_template',
                 'value' =>  array(
                     '1'   => get_template_directory_uri().'/vc_elements/layouts/post-1.png',
+                    '1_3'   => get_template_directory_uri().'/vc_elements/layouts/post-1_3.png',
                     '1_2' => get_template_directory_uri().'/vc_elements/layouts/post-1_2.png',
                     '2'   => get_template_directory_uri().'/vc_elements/layouts/post-2.png',
                     '3'   => get_template_directory_uri().'/vc_elements/layouts/post-3.png',
