7a60952bbac8003ad6dc6c78d4e4b0c82e582582
vannha <phongnhatk21@gmail.com>
2019-11-28 09:47:32 +0700

    commit -

--- inc/extensions/ef5-twitter.php
+++ inc/extensions/ef5-twitter.php
@@ -1,32 +1,33 @@
 <?php
-add_filter(
-	'ef5systems_twitter_api_consumer_key', 
-	overcome_get_theme_opt(
+$x = overcome_get_theme_opt(
 		'twitter_api_consumer_key',
 		'i90SevLFwZDscXPo3Wj89Y4eO'
-	)
-);
+	);
 add_filter(
-	'ef5systems_twitter_api_consumer_secret', 
-	overcome_get_theme_opt(
-		'twitter_api_consumer_secret',
-		'61AmOoAxacZeQneXjCOzKZGRwXwcRFgMsIhhYnQ5JTAOvMdlmL'
-	)
+	'ef5systems_twitter_api_consumer_key', 
+	$x
 );
-add_filter(
-	'ef5systems_twitter_api_access_key',
-	overcome_get_theme_opt(
-		'twitter_api_access_key',
-		'107960275-v9RLlUdpW7xW0wbh0Xtg8X2mVFbaCDtFNAs8vwAc'
-	)
-);
-add_filter(
-	'ef5systems_twitter_api_access_secret', 
-	overcome_get_theme_opt(
-		'twitter_api_access_secret',
-		'VewAXAcJEyDpqlrDfDO40HbRq6rzkYPEHgXz3WNhxAbSv'
-	)
-);
+// add_filter(
+// 	'ef5systems_twitter_api_consumer_secret', 
+// 	overcome_get_theme_opt(
+// 		'twitter_api_consumer_secret',
+// 		'61AmOoAxacZeQneXjCOzKZGRwXwcRFgMsIhhYnQ5JTAOvMdlmL'
+// 	)
+// );
+// add_filter(
+// 	'ef5systems_twitter_api_access_key',
+// 	overcome_get_theme_opt(
+// 		'twitter_api_access_key',
+// 		'107960275-v9RLlUdpW7xW0wbh0Xtg8X2mVFbaCDtFNAs8vwAc'
+// 	)
+// );
+// add_filter(
+// 	'ef5systems_twitter_api_access_secret', 
+// 	overcome_get_theme_opt(
+// 		'twitter_api_access_secret',
+// 		'VewAXAcJEyDpqlrDfDO40HbRq6rzkYPEHgXz3WNhxAbSv'
+// 	)
+// );
 
 /*add_filter('ef5systems_twitter_api_consumer_key', function () {return 'i90SevLFwZDscXPo3Wj89Y4eO';});
 add_filter('ef5systems_twitter_api_consumer_secret', function () {return '61AmOoAxacZeQneXjCOzKZGRwXwcRFgMsIhhYnQ5JTAOvMdlmL';});
