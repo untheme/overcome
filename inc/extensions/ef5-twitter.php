cfa9b3af095c01ff79227b2bb15e09b31fe72657
vannha <phongnhatk21@gmail.com>
2019-11-28 09:55:09 +0700

    commit -

--- inc/extensions/ef5-twitter.php
+++ inc/extensions/ef5-twitter.php
@@ -36,8 +36,3 @@ add_filter(
 		);
 	}
 );
-
-/*add_filter('ef5systems_twitter_api_consumer_key', function () {return 'i90SevLFwZDscXPo3Wj89Y4eO';});
-add_filter('ef5systems_twitter_api_consumer_secret', function () {return '61AmOoAxacZeQneXjCOzKZGRwXwcRFgMsIhhYnQ5JTAOvMdlmL';});
-add_filter('ef5systems_twitter_api_access_key',function() { return '107960275-v9RLlUdpW7xW0wbh0Xtg8X2mVFbaCDtFNAs8vwAc';});
-add_filter('ef5systems_twitter_api_access_secret', function () {return 'VewAXAcJEyDpqlrDfDO40HbRq6rzkYPEHgXz3WNhxAbSv';});*/
