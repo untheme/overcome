dcc9b7fd4a30978ba98b1ca1138a31d4ff1a9599
vannha <phongnhatk21@gmail.com>
2019-12-02 11:31:38 +0700

    commit -

---
+++ vc_elements/ef5_posts_masonry.php
@@ -0,0 +1,160 @@
+<?php
+vc_map(array(
+    'name'          => 'OverCome Posts Masonry',
+    'base'          => 'ef5_posts_masonry',
+    'category'      => esc_html__('OverCome', 'overcome'),
+    'description'   => esc_html__('Display your posts with Masonry layout', 'overcome'),
+    'icon'         => 'vc_icon-vc-masonry-grid',
+    'params'        => array_merge(
+        array(
+            array(
+                'type'        => 'dropdown',
+                'heading'     => esc_html__( 'Data source', 'overcome' ),
+                'param_name'  => 'post_type',
+                'value'       => overcome_vc_post_type_list(),
+                'std'         => 'post',
+                'description' => esc_html__( 'Select content type for your grid.', 'overcome' ),
+                'admin_label' => true,
+            ),
+            array(
+                'type'       => 'autocomplete',
+                'heading'    => esc_html__( 'Narrow data source', 'overcome' ),
+                'param_name' => 'taxonomies',
+                'settings'   => array(
+                    'multiple'       => true,
+                    'min_length'     => 1,
+                    'groups'         => true,
+                    'unique_values'  => true,
+                    'display_inline' => true,
+                    'delay'          => 500,
+                    'auto_focus'     => true,
+                    'values'         => ef5systems_taxonomies_for_autocomplete(),
+                ),
+                'description' => esc_html__( 'Enter categories, tags or custom taxonomies.', 'overcome' ),
+                'admin_label' => true,
+            ),
+            array(
+                'type'       => 'autocomplete',
+                'heading'    => esc_html__( 'Exclude from Content and filter list', 'overcome' ),
+                'param_name' => 'taxonomies_exclude',
+                'settings'   => array(
+                    'multiple'       => true,
+                    'min_length'     => 1,
+                    'groups'         => true,
+                    'unique_values'  => true,
+                    'display_inline' => true,
+                    'delay'          => 500,
+                    'auto_focus'     => true,
+                    'values'         => ef5systems_taxonomies_for_autocomplete(),
+                ),
+                'description' => esc_html__( 'Enter categories, tags or custom taxonomies won\'t be shown in the content and filters list', 'overcome' ),
+                'admin_label' => true
+            ),
+            array(
+                'type'          => 'textfield',
+                'param_name'    => 'posts_per_page',
+                'heading'       => esc_html__( 'Number of posts', 'overcome' ),
+                'description'   => esc_html__( 'number of post to show per page', 'overcome' ),
+                'std'           => '8',
+            ),
+            
+            array(
+                'type'        => 'el_id',
+                'settings' => array(
+                    'auto_generate' => true,
+                ),
+                'heading'     => esc_html__( 'Element ID', 'overcome' ),
+                'param_name'  => 'el_id',
+                'description' => sprintf( __( 'Enter element ID (Note: make sure it is unique and valid according to <a href="%s" target="_blank">w3c specification</a>).', 'overcome' ), '//w3schools.com/tags/att_global_id.asp' ),
+            ),
+            array(
+                'type'       => 'textfield',
+                'heading'    => esc_html__('Extra Class','overcome'),
+                'param_name' => 'el_class',
+                'value'      => '',
+                'description' => esc_html__('Style particular content element differently - add a class name and refer to it in custom CSS.', 'overcome'),
+            )
+        ),
+        // Layout Template
+        array(
+            array(
+                'type'       => 'img',
+                'heading'    => esc_html__('Layout Template','overcome'),
+                'param_name' => 'layout_template',
+                'value' =>  array(
+                    '1' => get_template_directory_uri().'/vc_extends/layouts/posts-masonry-1.png',
+                ),
+                'std'   => '1',
+                'group' => esc_html__('Layouts','overcome'),
+            ),
+            array(
+                'type'          => 'checkbox',
+                'param_name'    => 'show_pagination',
+                'value'         => array(
+                    esc_html__( 'Show Pagination', 'overcome' ) => '1'
+                ),
+                'std'           => '1',
+                'group'         => esc_html__('Layouts','overcome')
+            )
+        ),
+        // Filter
+        array(
+            array(
+                'type'       => 'checkbox',
+                'param_name' => 'show_filter',
+                'value'      => array(
+                    esc_html__('Show Filter','overcome') => '1'
+                ),
+                'std'   => '1',
+                'group' => esc_html__('Filter','overcome'),
+            ),
+            array(
+                'type'       => 'img',
+                'heading'    => esc_html__('Filter Template','overcome'),
+                'param_name' => 'filter_template',
+                'img_class'  => 'cols-1',
+                'value' =>  array(
+                    '1' => get_template_directory_uri().'/vc_elements/layouts/filter-1.png'
+                ),
+                'std'   => '1',
+                'dependency' => array(
+                    'element' => 'show_filter',
+                    'value'   => array('1')
+                ), 
+                'group' => esc_html__('Filter','overcome'),
+            ),
+            overcome_vc_content_align_opts([
+                'param_name' => 'filter_content_align',
+                'group'      => esc_html__('Filter','overcome'),
+                'dependency' => [
+                    'element' => 'filter_template',
+                    'value'   => array('2','3','4')
+                ]
+            ]),
+        ),
+        // Item Template
+        array(
+            array(
+                'type'       => 'img',
+                'heading'    => esc_html__('Items Template','overcome'),
+                'param_name' => 'item_template',
+                'value' =>  array(
+                    '2-1' => get_template_directory_uri().'/vc_elements/layouts/posts-2.png',
+                ),
+                'std'   => '2',
+                'group' => esc_html__('Item Layout','overcome'),
+            )
+        )
+    )
+));
+
+class WPBakeryShortCode_ef5_posts_masonry extends WPBakeryShortCode
+{
+    protected function content($atts, $content = null)
+    {
+        $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+        wp_enqueue_script( 'imagesloaded');
+        wp_enqueue_script( 'isotope');
+        return parent::content($atts, $content);
+    }
+}
---
+++ vc_templates/ef5_posts_masonry.php
@@ -0,0 +1,181 @@
+<?php
+    $lists = $icon = '';
+    $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+    extract( $atts );
+    $el_id = !empty($el_id) ? 'ef5-posts-'.$el_id : uniqid('ef5-posts-');
+    /* get value for Design Tab */
+    $css_classes = array(
+        'ef5-posts-masonry',
+        'ef5-masonry',
+        'ef5-posts-masonry-'.$layout_template,
+        'ef5-posts-grid-'.$item_template
+    );
+    
+    $css_class = preg_replace( '/\s+/', ' ', apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, implode( ' ', array_filter( $css_classes ) ), $this->settings['base'], $atts ) );
+
+    /* Post query */
+    $tax_query = overcome_tax_query($post_type, $taxonomies, $taxonomies_exclude);
+    if ( get_query_var('paged') ) {
+        $paged = get_query_var('paged');
+    } elseif ( get_query_var('page') ) {
+        $paged = get_query_var('page');
+    } else {
+        $paged = 1;
+    }
+    $posts_args = array(
+        'post_type'      => $post_type,
+        'posts_per_page' => $posts_per_page,
+        'post_status'    => 'publish',
+        'tax_query'      => $tax_query,
+        'paged'          => $paged,
+    );
+    global $wp_query;
+    $posts = $wp_query = new WP_Query($posts_args);
+    // Grid columns css class
+    $grid_item_css_class = ['ef5-masonry-item', 'ef5-grid-item','ef5-hover-wrap','hoverdir-wrap','fade-in'];
+    // Items CSS Classes
+    $item_css_class = ['post-masonry-item','transition', $this->getCSSAnimation($item_css_animation)];
+
+    $has_shadow = ['2','3','4','5','6','14'];
+    if(in_array($item_template, $has_shadow)) $item_css_class[] = 'ef5-box ef5-box-shadow-13';
+
+    $slide_overlay = ['8','9'];
+    if(in_array($item_template, $slide_overlay)) $item_css_class[] = 'ef5-line-corner-wrap';
+    
+    // Title 
+    $title_class = '';
+
+    // Masonry 
+    $originLeft = is_rtl() ? false : true;
+    $masonry_opts = array(
+        'itemSelector'    => '.ef5-masonry-item',
+        'columnWidth'     => '.ef5-masonry-sizer',
+        'gutter'          => '.ef5-masonry-gutter',
+        'percentPosition' => true,
+        'originLeft'      => $originLeft,
+        'horizontalOrder' => true,
+        
+    );
+    // Thumbnail Size 
+    $masonry_size_index = -1;
+    switch ($layout_template) {
+        default:
+            $masonry_size = ['370x240'];
+            break;
+    }
+    // Filters
+    $filters_class = ['ef5-filters', 'ef5-masonry-filters', 'ef5-filters-'.$filter_template, str_replace('text-', 'justify-content-lg-',$filter_content_align), $this->getCSSAnimation('fadeInUp')];
+    // Filter Button Data 
+    $filter_terms_args = [
+        'taxonomy' => overcome_get_custom_post_taxonomies( $post_type , 'cat'), 
+        'exclude'  => overcome_get_term_id_by_slug($post_type, 'cat', $taxonomies_exclude)
+    ];
+    if(!empty($taxonomies))  $filter_terms_args['slug'] = explode(',', $taxonomies);
+    $filter_terms = get_terms($filter_terms_args);
+?>
+<div class="ef5-posts" id="<?php echo esc_attr($el_id);?>">
+    <?php if($show_filter === '1'): ?>
+        <div class="<?php echo overcome_optimize_css_class(implode(' ', $filters_class));?>">
+            <div class="filter-item active" data-filter="*">
+                <span><?php esc_html_e('All','unbreak'); ?></span>
+            </div>
+            <?php 
+                foreach ($filter_terms as $term) {
+                    echo '<div class="filter-item" data-filter="'.esc_attr('.'.$term->slug).'"><span>'.esc_html($term->name).'</span><span class="d-none">'.$term->count.'</span></div>';
+                } 
+            ?>
+        </div>
+    <?php endif; ?>
+    <div class="<?php echo esc_attr(trim($css_class));?>" data-masonry="<?php echo esc_attr(json_encode($masonry_opts));?>">
+        <div class="ef5-masonry-sizer"></div>
+        <div class="ef5-masonry-gutter"></div>
+        <?php 
+            $d = 0; $large_item = [1,5,9,10]; $large_item_index = -1;
+            while($posts->have_posts()){
+                $d++;
+                $large_item_index++;
+                if($large_item_index >= count($large_item))
+                    $large_item_index = $large_item_index - count($large_item);
+                
+                if( $large_item[$large_item_index] === 0) {
+                    $item_w = 'large';
+                } elseif ($d % $large_item[$large_item_index] === 0){
+                    $item_w = 'large';
+                } else {
+                    $item_w = 'small';
+                }
+                $posts->the_post();
+                $filter_class = overcome_get_taxo_slug_as_css_class(['taxo' => overcome_get_custom_post_taxonomies( $post_type , 'cat')] );
+                // Thumbnail Size
+                $masonry_size_index++;
+                if($masonry_size_index >= count($masonry_size))
+                    $masonry_size_index = $masonry_size_index - count($masonry_size) ;
+                // Post Metas
+                $post_metas   = [];
+                $post_metas[] = overcome_posted_on(['show_date'=>'1','echo' => false]);
+                $post_metas[] = overcome_posted_by(['show_author'=>'1','author_avatar' => false, 'echo' => false]);
+                /**
+                 * Layout 5 Post Metas 
+                */
+                $post_metas_5   = [];
+                $post_metas_5[] = overcome_posted_by(['show_author'=>'1','author_avatar' => false, 'echo' => false]);
+                $post_metas_5[] = overcome_comments_popup_link(['show_cmt'=>'1','echo' => false]);
+
+                // Readmore button 
+                $overcome_post_media_readmore = overcome_post_read_more([
+                    'before'         => '<div class="center-align">',
+                    'after'          => '</div>',
+                    'echo'           => false, 
+                    'show_readmore'  => '1', 
+                    'readmore_class' => ''
+                ]);
+                $overcome_post_media_readmore2 = overcome_post_read_more_circle([
+                    'before'         => '<div class="center-align">',
+                    'after'          => '</div>',
+                    'echo'           => false, 
+                    'show_readmore'  => '1', 
+                    'readmore_class' => ''
+                ]);
+                $overcome_post_media_cat   = overcome_posted_in(['echo' => false, 'show_cat' => '1','class' => 'ef5-box-meta ef5-box-meta2', 'sep' => '']);
+                $overcome_post_media_share = overcome_post_share(['echo' => false, 'class' => 'col-auto', 'show_share' => '0', 'show_title' => false, 'social_args' => ['class' => 'shape-circle colored-hover outline justify-content-center', 'size' => '30']]);
+                $overcome_post_media_date  = overcome_posted_on(['echo' => false, 'show_date' => '1','class' => 'ef5-box-meta ef5-box-meta2 text-uppercase', 'sep' => '']);
+
+            ?>
+            <div class="<?php echo overcome_optimize_css_class(implode(' ',$grid_item_css_class).' '.$item_w.' '.$filter_class); ?>">
+            <?php  
+                switch ($item_template) {
+                    default:
+            ?>	
+            	<div class="<?php echo overcome_optimize_css_class(implode(' ', $item_css_class)); ?>">
+                    <?php 
+                        overcome_post_media([
+                            'thumbnail_size' => $masonry_size[$masonry_size_index], 
+                            'default_thumb'  => true
+                        ]);
+                    ?>
+                    <?php 
+                        overcome_post_header([
+                            'heading_tag' => overcome_default_value($heading_tag,'h2'), 
+                            'before_args' => ['show_cat'=> '1'], 
+                            'after_args'  => ['show_cat' => false,'show_author' => '1', 'show_date'=> '1', 'show_cmt' => '1', 'show_view' => '0', 'show_like' => '0', 'sep' => '|' ]]);
+
+                        overcome_post_excerpt([
+                            'show_excerpt' => '1', 
+                            'length'       => '15', 
+                            'more'         => ''
+                        ]);
+                        overcome_post_read_more(['show_readmore' => '1']); 
+                    ?>
+                </div>
+            <?php
+                    break;
+                }
+            ?>
+            </div>
+            <?php
+            } // end while
+            if($show_pagination !== '1') wp_reset_query();
+        ?>
+    </div>
+    <?php overcome_loop_pagination(['show_pagination' => $show_pagination, 'style' => '2']); ?>
+</div>
