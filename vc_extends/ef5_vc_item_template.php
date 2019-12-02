1ca5158eed4073747d724f62a2d9ac060315b95d
vannha <phongnhatk21@gmail.com>
2019-12-02 09:50:22 +0700

    commit -

--- vc_extends/ef5_vc_item_template.php
+++ vc_extends/ef5_vc_item_template.php
@@ -27,6 +27,44 @@ function overcome_vc_post_layout2($atts){
         </div>
 	<?php
 }
+function overcome_vc_post_layout2_1($atts){
+    $after = '<div class="overlay ef5-bg-overlay"><div class="overlay-inner center-align"><a class="text-36 text-white" href="'.get_the_permalink().'"><span class="fa fa-link"></span></a></div></div>';
+    ?>
+        <div class="relative">
+            <?php 
+                overcome_post_media([
+                    'thumbnail_size' => '', 
+                    'default_thumb'  => true,
+                    'after'          => $after
+                ]);
+            ?>
+        </div>
+        <div class="pl-15 pr-15 pl-lg-35 pr-lg-35 pt-25 pb-25">
+            <?php 
+                overcome_post_title([
+                    'heading_tag' => 'text-22',
+                    'class'       => 'pb-15'  
+                ]);
+                overcome_post_excerpt([
+                    'show_excerpt' => '1', 
+                    'length'       => '15', 
+                    'more'         => ''
+                ]);
+                //overcome_loop_donate_info();
+                if(class_exists('EF5Payments')) {
+                ef5systems_donation_progress_donors([
+                    'donor_icon' => '<span class="flaticon-like"></span>'
+                ]);
+                ef5payments_donation_donate_amount([
+                    'goal_label' => esc_html__('Goal:','overcome'),
+                    'raised_label' => esc_html__('Raised:','overcome')
+                ]);
+            }
+            ?>
+        </div>
+    <?php
+
+}
 
 function overcome_vc_post_layout6($atts, $args = []){
     $args = wp_parse_args($args,[
