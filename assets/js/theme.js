2d9ce35a7884effe2a66a680e8a8a11ef1a28a5d
vannha <phongnhatk21@gmail.com>
2019-12-09 14:15:44 +0700

    commit -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -763,7 +763,7 @@
     /* Tabs */
     function overcome_tabs(){
         'use strict';
-        $('.ef5-tabs-nav a').on('click', function (event) {
+        $('.ef5-tabs-nav .filter-item').on('click', function (event) {
             event.preventDefault();
             console.log($(this));
             $('.ef5-tab-active').removeClass('ef5-tab-active');
