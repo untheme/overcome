9c42391d0fb8fea6f1189857636c12181dcf4471
vannha <phongnhatk21@gmail.com>
2019-12-11 10:24:12 +0700

    commit -

--- inc/custom-post/donation.php
+++ inc/custom-post/donation.php
@@ -2,7 +2,7 @@
 /**
  * Change single donation navigation label
 */
-//var_dump(has_filter('ef5payments_loop_pagination_ef5_donation_prev_text')); die('cc12');
+var_dump(has_filter('ef5payments_loop_pagination_ef5_donation_prev_text'));
 add_filter('ef5payments_loop_pagination_ef5_donation_prev_text', function(){
 	return esc_html__('Prev Cause','overcome');
 });
