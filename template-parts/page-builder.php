25556144976edfe201ba66c9f1417caf05872868
vannha <phongnhatk21@gmail.com>
2019-12-12 08:56:26 +0700

    commit -

--- template-parts/page-builder.php
+++ template-parts/page-builder.php
@@ -23,6 +23,9 @@ get_header();
             ?>
         </div>
         <?php overcome_sidebar(); ?>
+        <?php tribe_events_before_html(); ?>
+    <?php tribe_get_view(); ?>
+    <?php tribe_events_after_html(); ?>
     </div>
 <?php
 get_footer();
