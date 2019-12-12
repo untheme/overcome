19ffc8ac2a89dbdebbcc3f803689fb200008b86c
vannha <phongnhatk21@gmail.com>
2019-12-12 11:09:19 +0700

    commit -

--- tribe-events/modules/bar.php
+++ tribe-events/modules/bar.php
@@ -40,8 +40,7 @@ if ( count( $views ) > 1 ) {
 	<form id="tribe-bar-form" class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>" name="tribe-bar-form" method="post" action="<?php echo esc_attr( $current_url ); ?>">
 
 		<?php if ( ! empty( $filters ) ) : ?>
-			<div id="tribe-bar-filters-wrap">
-
+			<div id="ef5-tribe-bar-filters-wrap">
 				<button
 					id="tribe-bar-collapse-toggle"
 					aria-expanded="false"
@@ -56,7 +55,7 @@ if ( count( $views ) > 1 ) {
 					<span class="tribe-bar-toggle-arrow"></span>
 				</button>
 
-				<div id="tribe-bar-filters" class="tribe-bar-filters" aria-hidden="true">
+				<div id="ef5-tribe-bar-filters" class="tribe-bar-filters" aria-hidden="true">
 					<div class="tribe-bar-filters-inner tribe-clearfix">
 						<h3 class="tribe-events-visuallyhidden"><?php printf( esc_html__( '%s Search', 'the-events-calendar' ), tribe_get_event_label_plural() ); ?></h3>
 						<?php foreach ( $filters as $filter ) : ?>
@@ -65,6 +64,33 @@ if ( count( $views ) > 1 ) {
 								<?php echo $filter['html'] ?>
 							</div>
 						<?php endforeach; ?>
+						<?php if ( count( $views ) > 1 ) : ?>
+							<div id="tribe-bar-views" class="ef5-tribe-bar-views">
+								<div class="tribe-bar-views-inner tribe-clearfix">
+									<h3 class="tribe-events-visuallyhidden"><?php printf( esc_html__( '%s Views Navigation', 'the-events-calendar' ), tribe_get_event_label_singular() ); ?></h3>
+									<label id="tribe-bar-views-label" aria-label="<?php printf( esc_html__( 'View %s As', 'the-events-calendar' ), tribe_get_event_label_plural() ); ?>">
+										<?php esc_html_e( 'View As', 'the-events-calendar' ); ?>
+									</label>
+									<select
+										class="tribe-bar-views-select tribe-no-param"
+										name="tribe-bar-view"
+										aria-label="<?php printf( esc_attr__( 'View %s As', 'the-events-calendar' ), tribe_get_event_label_plural() ); ?>"
+									>
+										<?php
+										foreach ( $views as $view ) {
+											printf(
+												'<option value="%1$s" data-view="%2$s"%3$s>%4$s</option>',
+												esc_attr( $view['url'] ),
+												esc_attr( $view['displaying'] ),
+												tribe_is_view( $view['displaying'] ) ? ' selected' : '',
+												esc_html( $view['anchor'] )
+											);
+										}
+										?>
+									</select>
+								</div>
+							</div>
+						<?php endif; ?>
 						<div class="tribe-bar-submit">
 							<input
 								class="tribe-events-button tribe-no-param"
@@ -77,37 +103,7 @@ if ( count( $views ) > 1 ) {
 					</div>
 				</div>
 			</div>
-
 		<?php endif; ?>
-
-		<?php if ( count( $views ) > 1 ) : ?>
-			<div id="tribe-bar-views" class="tribe-bar-views">
-				<div class="tribe-bar-views-inner tribe-clearfix">
-					<h3 class="tribe-events-visuallyhidden"><?php printf( esc_html__( '%s Views Navigation', 'the-events-calendar' ), tribe_get_event_label_singular() ); ?></h3>
-					<label id="tribe-bar-views-label" aria-label="<?php printf( esc_html__( 'View %s As', 'the-events-calendar' ), tribe_get_event_label_plural() ); ?>">
-						<?php esc_html_e( 'View As', 'the-events-calendar' ); ?>
-					</label>
-					<select
-						class="tribe-bar-views-select tribe-no-param"
-						name="tribe-bar-view"
-						aria-label="<?php printf( esc_attr__( 'View %s As', 'the-events-calendar' ), tribe_get_event_label_plural() ); ?>"
-					>
-						<?php
-						foreach ( $views as $view ) {
-							printf(
-								'<option value="%1$s" data-view="%2$s"%3$s>%4$s</option>',
-								esc_attr( $view['url'] ),
-								esc_attr( $view['displaying'] ),
-								tribe_is_view( $view['displaying'] ) ? ' selected' : '',
-								esc_html( $view['anchor'] )
-							);
-						}
-						?>
-					</select>
-				</div>
-			</div>
-		<?php endif; ?>
-
 	</form>
 
 </div>
