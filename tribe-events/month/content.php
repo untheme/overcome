76755873c9c9f7f534a6a4aa37670ccce44e249a
vannha <phongnhatk21@gmail.com>
2019-12-12 16:45:33 +0700

    commit -

--- tribe-events/month/content.php
+++ tribe-events/month/content.php
@@ -32,7 +32,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php do_action( 'tribe_events_after_header' ) ?>
 
 	<!-- Month Grid -->
-	<?php //tribe_get_template_part( 'month/loop', 'grid' ) ?>
+	<?php tribe_get_template_part( 'month/loop', 'grid' ) ?>
 
 	<!-- Month Footer -->
 	<?php do_action( 'tribe_events_before_footer' ) ?>
