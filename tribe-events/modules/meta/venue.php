9df422252c9db0249e3a9274cbc0b9657d5a7dd1
vannha <phongnhatk21@gmail.com>
2019-12-18 15:16:54 +0700

    commit -

--- tribe-events/modules/meta/venue.php
+++ tribe-events/modules/meta/venue.php
@@ -19,34 +19,33 @@ $website = tribe_get_venue_website_link();
 ?>
 
 <div class="tribe-events-meta-group tribe-events-meta-group-venue">
-	<h2 class="tribe-events-single-section-title"> <?php esc_html_e( tribe_get_venue_label_singular(), 'the-events-calendar' ) ?> </h2>
-	<dl>
 		<?php do_action( 'tribe_events_single_meta_venue_section_start' ) ?>
+		<div class="row">
+			<div class="col-auto"><?php esc_html_e( 'Venue:', 'overcome' ) ?></div>
+			<div class="col text-end">
+				<?php echo tribe_get_venue() ?>
+				<?php if ( tribe_address_exists() ) : ?>
+					<address class="tribe-events-address">
+						<?php echo tribe_get_full_address(); ?>
 
-		<dd class="tribe-venue"> <?php echo tribe_get_venue() ?> </dd>
-
-		<?php if ( tribe_address_exists() ) : ?>
-			<dd class="tribe-venue-location">
-				<address class="tribe-events-address">
-					<?php echo tribe_get_full_address(); ?>
-
-					<?php if ( tribe_show_google_map_link() ) : ?>
-						<?php echo tribe_get_map_link_html(); ?>
-					<?php endif; ?>
-				</address>
-			</dd>
-		<?php endif; ?>
-
-		<?php if ( ! empty( $phone ) ): ?>
-			<dt class="tribe-venue-tel-label"> <?php esc_html_e( 'Phone:', 'the-events-calendar' ) ?> </dt>
-			<dd class="tribe-venue-tel"> <?php echo $phone ?> </dd>
-		<?php endif ?>
-
-		<?php if ( ! empty( $website ) ): ?>
-			<dt class="tribe-venue-url-label"> <?php esc_html_e( 'Website:', 'the-events-calendar' ) ?> </dt>
-			<dd class="tribe-venue-url"> <?php echo $website ?> </dd>
-		<?php endif ?>
-
+						<?php if ( tribe_show_google_map_link() ) : ?>
+							<?php echo tribe_get_map_link_html(); ?>
+						<?php endif; ?>
+					</address>
+				<?php endif; ?>
+				<?php if ( ! empty( $phone ) ): ?>
+				<div class="row">
+					<div class="col-auto"><?php esc_html_e( 'Phone:', 'the-events-calendar' ) ?></div>
+					<div class="col text-end"><?php echo $phone ?></div>
+				</div>
+				<?php endif ?>
+				<?php if ( ! empty( $website ) ): ?>
+				<div class="row">
+					<div class="col-auto"><?php esc_html_e( 'Website:', 'the-events-calendar' ) ?></div>
+					<div class="col text-end"><?php echo $website ?></div>
+				</div>
+				<?php endif ?>
+			</div>
+		</div>
 		<?php do_action( 'tribe_events_single_meta_venue_section_end' ) ?>
-	</dl>
 </div>
