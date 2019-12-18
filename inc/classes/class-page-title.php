bdc1b57188cd06a8e92e54481e5d6ff4e01fdcb4
vannha <phongnhatk21@gmail.com>
2019-12-18 08:40:52 +0700

    commit -

--- inc/classes/class-page-title.php
+++ inc/classes/class-page-title.php
@@ -38,6 +38,7 @@ function overcome_get_page_titles()
         }
         // Single page view
         elseif (is_singular()) {
+            die('xx1');
             $title = get_post_meta(get_the_ID(), 'custom_title', true);
             if (!$title) {
                 $title = get_the_title();
