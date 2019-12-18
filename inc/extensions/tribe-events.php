29c13b16a95caf47846a030b953bc966f30ebe98
vannha <phongnhatk21@gmail.com>
2019-12-18 16:08:02 +0700

    commit -

--- inc/extensions/tribe-events.php
+++ inc/extensions/tribe-events.php
@@ -14,99 +14,78 @@ if(function_exists('tribe')){
 // Single Event Upcoming
 add_action('tribe_events_single_meta_after', 'overcome_single_tribe_event_upcoming', 10, 1);
 function overcome_single_tribe_event_upcoming(){
-	//use Tribe__Date_Utils as Dates;
-	$query_args = [
-		'eventDisplay'         => 'list',
-		'posts_per_page'       => '3',
-		'is_tribe_widget'      => false,
-		'post_status'          => 'publish',
-		'tribe_render_context' => 'content',
-		'featured'             => false,
-		'ends_after'           => '',
-	];
+	$r = new WP_Query( array(
+        'post_type'           => 'tribe_events',
+        'posts_per_page'      => '3',
+        'no_found_rows'       => true,
+        'post_status'         => 'publish',
+        'ignore_sticky_posts' => true
+    ) );
 
-	$posts = tribe_get_events( $query_args, true );
-	//var_dump($query);
-	//$posts = $query->posts;
-	//var_dump($posts);
-// Check if any event posts are found.
-if ( $posts ) : ?>
+    if ( $r->have_posts() )
+    {
+        echo '<div class="posts-list layout-'.esc_attr($layout).'">';
 
-	<ol class="tribe-list-widget">
-		<?php
-		// Setup the post data for each event.
-		foreach ( $posts as $post ) :
-			setup_postdata( $post );
-			?>
-			<li class="tribe-events-list-widget-events <?php tribe_events_event_classes() ?>">
-				<?php
-				if (
-					tribe( 'tec.featured_events' )->is_featured( get_the_ID() )
-					&& get_post_thumbnail_id( $post )
-				) {
-					/**
-					 * Fire an action before the list widget featured image
-					 */
-					do_action( 'tribe_events_list_widget_before_the_event_image' );
+        while ( $r->have_posts() )
+        {
+            $r->the_post();
 
-					/**
-					 * Allow the default post thumbnail size to be filtered
-					 *
-					 * @param $size
-					 */
-					$thumbnail_size = apply_filters( 'tribe_events_list_widget_thumbnail_size', 'post-thumbnail' );
+            printf(
+                '<div class="post-list-item transition %s"><div class="row gutters-20">',
+                ( has_post_thumbnail() ? 'has-post-thumbnail' : '' )
+            );
 
-					/**
-					 * Filters whether the featured image link should be added to the Events List Widget
-					 *
-					 * @since 4.5.13
-					 *
-					 * @param bool $featured_image_link Whether the featured image link should be added or not
-					 */
-					$featured_image_link = apply_filters( 'tribe_events_list_widget_featured_image_link', true );
-					$post_thumbnail      = get_the_post_thumbnail( null, $thumbnail_size );
+            
+            $thumbnail_url = overcome_get_image_url_by_size([
+                'size'          => $thumbnail_size,
+                'default_thumb' => true,
+            ]);
+            printf(
+                '<div class="ef5-featured col-auto">' .
+                    '<a href="%1$s" title="%2$s" class="ef5-thumbnail">' .
+                        '<img src="%3$s" alt="%2$s" />' .
+                    '</a>' .
+                '</div>',
+                esc_url( get_permalink() ),
+                esc_attr( get_the_title() ),
+                esc_url( $thumbnail_url )
+            );
 
-					if ( $featured_image_link ) {
-						$post_thumbnail = '<a href="' . esc_url( tribe_get_event_link() ) . '">' . $post_thumbnail . '</a>';
-					}
-					?>
-					<div class="tribe-event-image">
-						<?php
-						// not escaped because it contains markup
-						echo $post_thumbnail;
-						?>
-					</div>
-					<?php
+            echo '<div class="ef5-brief col" style="max-width: calc(100% - '.$thumbnail_size[0].'px);">';
 
-					/**
-					 * Fire an action after the list widget featured image
-					 */
-					do_action( 'tribe_events_list_widget_after_the_event_image' );
-				}
-				?>
+            printf(
+                '<h4 class="ef5-heading font-style-500 pb-10"><a href="%1$s" title="%2$s">%3$s</a></h4>',
+                esc_url( get_permalink() ),
+                esc_attr( get_the_title() ),
+                get_the_title()
+            );
+            
+            overcome_tribe_events_time([
+                'before' => '<div class="text-15 font-style-500 ef5-text-primary">',
+                'after'  => '</div>'
+            ]);
+            if ( $show_author || $show_comments || $show_date || $show_cat )
+            {
+                ob_start();
+                if($show_author) overcome_posted_by();
+                if($show_date) overcome_posted_on();
+                if($show_comments) overcome_comments_popup_link(['show_text'=> true]);
+                if($show_cat) overcome_posted_in();
+                $post_meta = ob_get_clean();
 
-				<?php do_action( 'tribe_events_list_widget_before_the_event_title' ); ?>
-				<!-- Event Title -->
-				<h4 class="tribe-event-title">
-					<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h4>
+                if ( $post_meta )
+                {
+                    printf( '<div class="ef5-meta row gutter-20 justify-content-between">%s</div>', $post_meta );
+                }
+            }
+            echo '</div>';
 
-				<?php do_action( 'tribe_events_list_widget_after_the_event_title' ); ?>
-				<!-- Event Time -->
+            echo '</div></div>';
+        } // while
 
-				<?php do_action( 'tribe_events_list_widget_before_the_meta' ) ?>
-
-				<div class="tribe-event-duration">
-					<?php echo tribe_events_event_schedule_details(); ?>
-				</div>
-
-				<?php do_action( 'tribe_events_list_widget_after_the_meta' ) ?>
-			</li>
-		<?php
-		endforeach; ?>
-	</ol>
-	<?php
-endif;
+        echo '</div>';
+    } // have_posts
+    wp_reset_postdata();
 }
 // Single Events Tags
 add_action('tribe_events_single_meta_after','overcome_single_tribe_event_tags', 10, 2);
