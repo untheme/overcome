b7042371d310da12551938c8815422c7e4470ccf
vannha <phongnhatk21@gmail.com>
2019-12-19 16:47:23 +0700

    commit -

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -46,7 +46,7 @@ $event_id = get_the_ID();
 							<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( esc_html__('Next Event').'<span></span>' ) ?></li>
 						</ul>
 					</nav>
-					<div class="xxx">123<?php 
+					<div class="xxx">12345<?php 
 						if ( get_post_type() == Tribe__Events__Main::POSTTYPE && tribe_get_option( 'showComments', false ) ) { //comments_template();
 						}
 					?></div>
