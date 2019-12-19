017cc652890208c6e555b30dfee5b76cc13976f4
vannha <phongnhatk21@gmail.com>
2019-12-19 17:07:31 +0700

    commit -

---
+++ template-parts/page-single-tribe-event.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: OverCome Tribe Event
+ *
+ * This is the template that displays page content with VC.
+ *
+ * @package EF5 Theme
+ * @subpackage OverCome
+ * @since 1.0.0
+ * @author EF5 Team
+ */
+get_header();
+?>
+    <div class="container">
+        <div class="row gutter-lg-60">
+            <div id="ef5-content-area" class="<?php overcome_content_css_class();?>">
+                <div id="ef5-posts" class="ef5-posts ef5-blogs">
+                <?php
+					while ( have_posts() ) :
+						the_post();
+                        overcome_post_content();
+					endwhile;
+                ?>
+                </div>
+            </div>
+            <?php overcome_sidebar(); ?>
+        </div>
+    </div>
+<?php
+get_footer();
