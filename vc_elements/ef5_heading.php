b44c22b6f8bcec376aaad76f579dab43fa0956b4
vannha <phongnhatk21@gmail.com>
2019-12-20 16:39:33 +0700

    commit -

--- vc_elements/ef5_heading.php
+++ vc_elements/ef5_heading.php
@@ -487,7 +487,12 @@ class WPBakeryShortCode_ef5_heading extends WPBakeryShortCode
             'before' => '<div class="ef5-heading-btn">',
             'after'  => '</div>',
             'class'  => ''
+            'class2'  => ''
         ]);
+        $args['class'] .= $this->getCSSAnimation( $atts['button_link_css_animation'] );
+
+        if(empty($args['class2'])) $args['class2'] = $args['class'];
+        $args['class2'] .= $this->getCSSAnimation( $atts['button_link2_css_animation'] );
         extract( $atts );
         //  Button Link
         $use_link = false;
@@ -511,7 +516,7 @@ class WPBakeryShortCode_ef5_heading extends WPBakeryShortCode
             $html = str_replace('class="', 'class="'.$args['wrap_class'].' ', $args['before']);
                 $html .= '<a href="'.esc_url($a_href).'" class="'.$args['class'].'" target="'.esc_attr($a_target).'">'.$a_title.'</a>';
                 if($use_link2){
-                    $html .= '<span class="text-bt">'.esc_html($button_text_between).'<span><a href="'.esc_url($a_href2).'" class="'.$args['class'].'" target="'.esc_attr($a_target2).'">'.$a_title2.'</a>';
+                    $html .= '<span class="text-bt">'.esc_html($button_text_between).'<span><a href="'.esc_url($a_href2).'" class="'.$args['class2'].'" target="'.esc_attr($a_target2).'">'.$a_title2.'</a>';
                 }
             $html .= $args['after'];
             echo overcome_html($html);
