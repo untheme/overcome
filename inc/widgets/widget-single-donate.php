53311089e3e7b52397f0bb841295dce84f3791f1
vannha <phongnhatk21@gmail.com>
2019-12-24 09:55:43 +0700

    commit -

--- inc/widgets/widget-single-donate.php
+++ inc/widgets/widget-single-donate.php
@@ -103,7 +103,7 @@ class OverCome_WG_Single_Donate extends WP_Widget
                         'label_class' => 'font-style-500',
                         'value_class' => 'text-40 ef5-text-accent font-style-600 d-block'
                     ]);
-                    echo '<div class="row grid-gutter-20">';
+                    echo '<div class="row grid-gutter-20 align-items-center">';
                         echo '<div class="col">';
                             ef5payments_donation_donate_button([
                                 'class' => 'ef5-btn accent fill'
