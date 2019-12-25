578c3dcba1ee1dc75cf2b11b1dbad0d03c6539d7
vannha <phongnhatk21@gmail.com>
2019-12-25 10:18:13 +0700

    commit -

--- inc/widgets/widget-recent-post.php
+++ inc/widgets/widget-recent-post.php
@@ -74,7 +74,7 @@ class OverCome_Recent_Posts_Widget extends WP_Widget
             'no_found_rows'       => true,
             'post_status'         => 'publish',
             'ignore_sticky_posts' => true,
-            'post__not_in'        => get_the_ID()  
+            'post__not_in'        => [get_the_ID()]  
         ) );
 
         if ( $r->have_posts() )
