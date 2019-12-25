e3495faa411ed9358626a0470e48f9bcf4df9e1d
vannha <phongnhatk21@gmail.com>
2019-12-25 09:57:43 +0700

    commit -

--- inc/functions/header-donate.php
+++ inc/functions/header-donate.php
@@ -2,7 +2,7 @@
 function overcome_header_donate_button(){
 	if(!class_exists('EF5Payments') || overcome_get_opts('header_donate', '0') === '0') return;
     wp_enqueue_script('bootstrap');
-    if(is_singular('ef5_donation')){
+    if(in_array(get_post_type(), ef5payments_supported_post_type())){
       $post_id = get_the_ID();
     } else {
       $post_id = ef5payments_default_donation(overcome_get_id_by_slug(overcome_get_opts('header_donate_item',''),'ef5_donation'));
