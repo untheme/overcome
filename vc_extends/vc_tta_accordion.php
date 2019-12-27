ec52d28ca15177a9c5a68f1f8ba40a0c4ffa8644
vannha <phongnhatk21@gmail.com>
2019-12-27 10:45:50 +0700

    commit -

--- vc_extends/vc_tta_accordion.php
+++ vc_extends/vc_tta_accordion.php
@@ -16,6 +16,11 @@ function overcome_vc_tta_accordion_params() {
     $param['value'][esc_html__('OverCome','overcome')] = 'overcome';
     $param['std'] = 'overcome';
     vc_update_shortcode_param('vc_tta_accordion', $param);
+    // Space 
+    $param = WPBMap::getParam('vc_tta_accordion', 'space');
+    $param['value'][esc_html__('OverCome','overcome')] = 'overcome';
+    $param['std'] = 'overcome';
+    vc_update_shortcode_param('vc_tta_accordion', $param);
     // Gap 
     $param = WPBMap::getParam('vc_tta_accordion', 'gap');
     $param['value'][esc_html__('OverCome','overcome')] = 'overcome';
