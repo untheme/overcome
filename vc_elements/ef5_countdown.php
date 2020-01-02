29e8c6dd4a69faa1f73162ff28f35b8bc743cd49
vannha <phongnhatk21@gmail.com>
2020-01-02 10:41:48 +0700

    commit -

--- vc_elements/ef5_countdown.php
+++ vc_elements/ef5_countdown.php
@@ -16,8 +16,8 @@ vc_map(array(
             'heading'    => esc_html__('Layout Mode','overcome'),
             'param_name' => 'layout_template',
             'value'      =>  array(
-                '1' => get_template_directory_uri().'/vc_extends/layouts/countdown-1.png',
-                '2' => get_template_directory_uri().'/vc_extends/layouts/countdown-2.png',
+                '1' => get_template_directory_uri().'/vc_elements/layouts/countdown-1.png',
+                '2' => get_template_directory_uri().'/vc_elements/layouts/countdown-2.png',
             ),
             'std'        => '1',
             'admin_label' => true
