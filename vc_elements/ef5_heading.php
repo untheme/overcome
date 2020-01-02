ecb5a6781da464fd91cbc103f46e6564d1750c09
vannha <phongnhatk21@gmail.com>
2020-01-02 15:04:12 +0700

    commit -

--- vc_elements/ef5_heading.php
+++ vc_elements/ef5_heading.php
@@ -320,9 +320,9 @@ class WPBakeryShortCode_ef5_heading extends WPBakeryShortCode
                 $text_size = ef5systems_vc_text_style_css($atts, [
                         'param_name' => [
                             'color'       => 'small_heading_text_color_opts',
-                            'font_size'   => 'small_heading_size',
-                            'font_style'  => 'small_heading_font_style',
-                            'line_height' => 'small_heading_line_height',
+                            'size'   => 'small_heading_size',
+                            'style'  => 'small_heading_font_style',
+                            'lh' => 'small_heading_line_height',
                         ],
                         'default' => [
                             'size'  => '22 text-md-36 text-lg-54',
