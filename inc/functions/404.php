97f70d1e57de768e2fc3fda0ad3fddfccfc0df6c
vannha <phongnhatk21@gmail.com>
2020-01-03 09:19:46 +0700

    commit -

--- inc/functions/404.php
+++ inc/functions/404.php
@@ -1,6 +1,7 @@
 <?php
 add_action('overcome_before_content','overcome_404_bg');
 function overcome_404_bg(){
+	if(!is_404()) return;
 	?>
 		<div class="ef5-404-img">
 			<img class="ef5-img-fit" src="<?php echo get_template_directory_uri();?>/assets/images/404.jpg" alt="404" />
