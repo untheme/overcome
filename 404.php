4921f1e3cea5e37ffc600b742d9e2660ed4399d3
vannha <phongnhatk21@gmail.com>
2020-01-03 08:45:29 +0700

    commit -

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ get_header();
 ?>	
 	<div class="container d-flex justify-content-center text-center">
 		<div class="error-404 not-found">
-			<div class="ef5-404-img">
+			<div class="ef5-404-img img-fit">
 				<img src="<?php echo get_template_directory_uri();?>/assets/images/404.jpg" alt="404" />
 			</div>
 			<div class="font-style-700 text-uppercase err-msg-large">
