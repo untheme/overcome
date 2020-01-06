46ec2247ba1a59301a25466cc236eef99af6d8bd
vannha <phongnhatk21@gmail.com>
2020-01-06 16:04:31 +0700

    commit -

--- vc_extends/vc_row.php
+++ vc_extends/vc_row.php
@@ -3,6 +3,5 @@ add_action('vc_after_init', 'overcome_vc_row_custom_params');
 function overcome_vc_row_custom_params() {   
     $param = WPBMap::getParam('vc_row', 'full_width');
     $param['value'][esc_html__('Stretch Row and Content Wide','overcome')] = 'stretch_row_content2';
-    $param['value'][esc_html__('Stretch Row and Content Wide 3','overcome')] = 'stretch_row_content3';
     vc_update_shortcode_param('vc_row', $param);
 }
