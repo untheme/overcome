1aedc50e154984274391d3b4bdca18020beaf79b
vannha <phongnhatk21@gmail.com>
2020-01-06 14:36:11 +0700

    commit -

--- vc_elements/ef5_quickcontact.php
+++ vc_elements/ef5_quickcontact.php
@@ -189,12 +189,12 @@ class WPBakeryShortCode_ef5_quickcontact extends WPBakeryShortCode
 
         vc_icon_element_fonts_enqueue( $value['i_type'] );
         $iconClass  = isset($value['i_icon_'. $value['i_type']]) ? $value['i_icon_'. $value['i_type']] : '';
-        $qc_icon    = !empty($iconClass) ? '<span class="'.trim(implode(' ', $icon_classes)).' '.$iconClass.'"></span>' : '';
+        $qc_icon    = !empty($iconClass) ? '<span class="'.overcome_optimize_css_class(implode(' ', $icon_classes)).' '.$iconClass.'"></span>' : '';
         $qc_heading = isset($value['qc_heading']) && !empty($value['qc_heading']) ? $value['qc_heading'] : '';
         $qc_text    = isset($value['qc_text']) ? $value['qc_text'] : '';
 
-        echo '<div class="'.trim(implode(' ', $item_class)).'">';
-            echo '<div class="'.trim(implode(' ', $item_inner_class)).'">';
+        echo '<div class="'.overcome_optimize_css_class(implode(' ', $item_class)).'">';
+            echo '<div class="'.overcome_optimize_css_class(implode(' ', $item_inner_class)).'">';
                 if(!empty($iconClass)) echo '<div class="col-auto">'.overcome_html($qc_icon).'</div>';
                 echo '<div class="col"><div class="row gutter-20">';
                     echo '<div class="qc-item-heading col-auto">'.overcome_html($qc_heading).'</div>';
