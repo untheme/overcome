11fd8e1d423f03b8bed9de3399a556a0158f1fd6
vannha <phongnhatk21@gmail.com>
2020-01-08 11:21:46 +0700

    commit -

--- inc/woo/_hook_image.php
+++ inc/woo/_hook_image.php
@@ -10,9 +10,6 @@
 
 
 function overcome_wc_thumbnail_value($value){
-    var_dump(overcome_get_opts('product_single_image_size',[]));
-
-var_dump(overcome_get_opts('product_loop_image_size',[]));
     $image_size_single_width  = overcome_configs('overcome_product_single_image_w');
     $image_size_single_height = overcome_configs('overcome_product_single_image_h');
 
