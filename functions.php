c8d79d0c26387539b6c4516303eeb0fe8c2da672
vannha <phongnhatk21@gmail.com>
2020-01-08 10:58:40 +0700

    commit -

--- functions.php
+++ functions.php
@@ -585,3 +585,5 @@ if(class_exists('WooCommerce')){
 overcome_require_folder('inc/extensions', get_template_directory());
 
 var_dump(overcome_get_opts('product_single_image_size'));
+
+var_dump(overcome_get_opts('product_loop_image_size'));
