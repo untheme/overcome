a5e193299a7cd276420f5b5fa156198cf34b7496
vannha <phongnhatk21@gmail.com>
2020-01-08 10:49:31 +0700

    commit -

--- functions.php
+++ functions.php
@@ -203,7 +203,7 @@ if (!function_exists('overcome_setup')) {
     }
     add_action('after_setup_theme', 'overcome_setup');
 }
-
+var_dump(overcome_configs('overcome_product_single_image_w'));
 function overcome_update(){
     /* Change default image thumbnail sizes in wordpress */
     $thumbnail_size = array(
