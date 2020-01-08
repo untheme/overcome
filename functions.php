7b2c0258de4657cbfa3a50a458bd4f2c08efb61c
vannha <phongnhatk21@gmail.com>
2020-01-08 10:21:27 +0700

    commit -

--- functions.php
+++ functions.php
@@ -185,7 +185,18 @@ if (!function_exists('overcome_setup')) {
         ));
 
         /* WooCommerce */
-        add_theme_support('woocommerce');
+        //add_theme_support('woocommerce');
+        add_theme_support( 'woocommerce', apply_filters( 'overcome_woocommerce_args', array(
+ 'single_image_width'    => 416,
+ 'thumbnail_image_width' => 324,
+ 'product_grid'          => array(
+ 'default_columns' => 3,
+ 'default_rows'    => 4,
+ 'min_columns'     => 1,
+ 'max_columns'     => 6,
+ 'min_rows'        => 1
+ )
+) ) );
         add_theme_support('wc-product-gallery-zoom');
         add_theme_support('wc-product-gallery-lightbox');
         add_theme_support('wc-product-gallery-slider');    
