8154a66aebbd51daddebcbc346fc24534e64a7e2
vannha <phongnhatk21@gmail.com>
2020-01-10 15:52:28 +0700

    commit -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -32,7 +32,7 @@
         // cart page
         overcome_table_cart_content();
         //overcome_table_move_column(table, selected ,from, to, remove, colspan, colspan_value);
-        //overcome_table_move_column('.woocommerce-cart-form__contents', '.woocommerce-cart-form__cart-item' ,1, 6, '', '', '');
+        overcome_table_move_column('.woocommerce-cart-form__contents', '.woocommerce-cart-form__cart-item' ,0, 5, '', '', '');
     });
 	// On Load 
 	$(window).load(function() {
