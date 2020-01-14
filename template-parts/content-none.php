11d3c4465b330992e0eebb812e35130c94e70343
vannha <phongnhatk21@gmail.com>
2020-01-14 11:16:53 +0700

    commit -

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,46 +9,44 @@
  *
 */
 ?>	
-	<div class="container">
-		<div class="no-content not-found">
-			<header class="page-header">
-				<h1><?php esc_html_e( 'Nothing Found', 'overcome' ); ?></h1>
-			</header>
+<div class="no-content not-found">
+	<header class="page-header">
+		<h1><?php esc_html_e( 'Nothing Found', 'overcome' ); ?></h1>
+	</header>
 
-			<div class="page-content">
-				<?php
-				if ( is_home() && current_user_can( 'publish_posts' ) ) :
+	<div class="page-content">
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
-					printf(
-						'<p>' . wp_kses(
-							/* translators: 1: link to WP admin new post page. */
-							__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'overcome' ),
-							array(
-								'a' => array(
-									'href' => array(),
-								),
-							)
-						) . '</p>',
-						esc_url( admin_url( 'post-new.php' ) )
-					);
+			printf(
+				'<p>' . wp_kses(
+					/* translators: 1: link to WP admin new post page. */
+					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'overcome' ),
+					array(
+						'a' => array(
+							'href' => array(),
+						),
+					)
+				) . '</p>',
+				esc_url( admin_url( 'post-new.php' ) )
+			);
 
-				elseif ( is_search() ) :
-					?>
+		elseif ( is_search() ) :
+			?>
 
-					<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'overcome' ); ?></p>
-					<?php
-					get_search_form();
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'overcome' ); ?></p>
+			<?php
+			get_search_form();
 
-				else :
-					?>
+		else :
+			?>
 
-					<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'overcome' ); ?></p>
-					<?php
-					get_search_form();
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'overcome' ); ?></p>
+			<?php
+			get_search_form();
 
-				endif;
+		endif;
 
-				?>
-			</div>
-		</div>
+		?>
 	</div>
+</div>
