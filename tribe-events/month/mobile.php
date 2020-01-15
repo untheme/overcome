dc31f6b80bbb9e9559006793a970ba03e15cc714
vannha <phongnhatk21@gmail.com>
2020-01-15 15:28:27 +0700

    commit -

--- tribe-events/month/mobile.php
+++ tribe-events/month/mobile.php
@@ -18,26 +18,26 @@
 
 <script type="text/html" id="tribe_tmpl_month_mobile">
 	<div class="tribe-events-mobile tribe-clearfix tribe-events-mobile-event-[[=eventId]][[ if(categoryClasses.length) { ]] [[= categoryClasses]][[ } ]]">
-		<h4 class="summary">
+		<div class="ef5-heading mb-20">
 			<a class="url" href="[[=permalink]]" title="[[=title]]" rel="bookmark">[[=raw title]]<\/a>
-		<\/h4>
+		<\/div>
 
 		<div class="tribe-events-event-body">
 			[[ if(imageSrc.length) { ]]
-			<div class="tribe-events-event-imagex">
+			<div class="tribe-events-event-imagex mb-20">
 				<a href="[[=permalink]]" title="[[=title]]">
 					<img src="[[=imageSrc]]" alt="[[=title]]" title="[[=title]]">
 				<\/a>
 			<\/div>
 			[[ } ]]
-			<div class="tribe-events-event-schedule-details">
+			<div class="tribe-events-event-schedule-detailsx mb-20">
 				<span class="tribe-event-date-start">[[=dateDisplay]] <\/span>
 			<\/div>
 			
 			[[ if(excerpt.length) { ]]
-			<div class="tribe-event-description"> [[=raw excerpt]] <\/div>
+			<div class="tribe-event-description mb-20"> [[=raw excerpt]] <\/div>
 			[[ } ]]
-			<a href="[[=permalink]]" class="tribe-events-read-more" rel="bookmark">[[=i18n.find_out_more]]<\/a>
+			<a href="[[=permalink]]" class="tribe-events-read-more ef5-btn accent fill" rel="bookmark">[[=i18n.find_out_more]]<\/a>
 		<\/div>
 	<\/div>
 </script>
