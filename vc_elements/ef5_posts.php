1d21924daec4b99409a8ea4199cbab29a30ddf3c
vannha <phongnhatk21@gmail.com>
2020-01-18 04:47:37 +0700

    commit -

--- vc_elements/ef5_posts.php
+++ vc_elements/ef5_posts.php
@@ -486,7 +486,7 @@ class WPBakeryShortCode_ef5_posts extends WPBakeryShortCode
                         'thumbnail_size' => $args['thumbnail_size'], 
                         'default_thumb'  => true,
                         'after'          => $overlay_content,
-                        'img_class'      => 'ef5-rounded-10 minh-380 w-100',
+                        'img_class'      => 'ef5-rounded-10 minh-sm-380 minh-auto w-100',
                         //'show_image'     => false
                     ]);   
                 ?>
