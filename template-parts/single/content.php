df6036bd7399fb8e7e2036123e9316a73913c153
vannha <phongnhatk21@gmail.com>
2020-01-18 03:05:26 +0700

    commit -

--- template-parts/single/content.php
+++ template-parts/single/content.php
@@ -12,21 +12,22 @@
 
 <div <?php post_class('ef5-single clearfix'); ?>>
     <?php 
-        //overcome_post_header(['class' => 'ef5-single-header']);
-        overcome_post_media([
-            'class'     => 'mb-40',
-            'img_class' => 'ef5-rounded-10 ef5-overlay-gradient-1',
-            'after'     => '<div class="single-post-meta row justify-content-between grid-gutters-20 text-white ef5-link-inherit"><div class="col">'.overcome_post_meta([
-                'class'         => 'text-white ef5-link-inherit',
-                'echo'          => false,
-                'show_cmt_text' => false
-            ]).'</div><div class="col-12 col-md-auto">' . overcome_post_share([
-                'class'      => 'text-12 size-small', 
-                'echo'       => false,
-                'show_title' => false,
-                'show_all'   => 'false',
-            ]).'</div></div>'
-        ]); 
+        if(has_post_thumbnail() || overcome_config('overcome_default_post_thumbnail')){
+            overcome_post_media([
+                'class'     => 'mb-40',
+                'img_class' => 'ef5-rounded-10 ef5-overlay-gradient-1',
+                'after'     => '<div class="single-post-meta row justify-content-between grid-gutters-20 text-white ef5-link-inherit"><div class="col">'.overcome_post_meta([
+                    'class'         => 'text-white ef5-link-inherit',
+                    'echo'          => false,
+                    'show_cmt_text' => false
+                ]).'</div><div class="col-12 col-md-auto">' . overcome_post_share([
+                    'class'      => 'text-12 size-small', 
+                    'echo'       => false,
+                    'show_title' => false,
+                    'show_all'   => 'false',
+                ]).'</div></div>'
+            ]);
+        }
         overcome_post_content(['class' => 'ef5-single-content']);
         overcome_link_pages(['class' => 'ef5-single-page-links']);
     ?>
